4a428718c877d4a5760e86c95d5f0daf074f2974
TolReed <tolreed@gmail.com>
2019-07-15 19:59:42 +0300

    2 tasks has been done here - cheers

--- 1.JavaSyntax/src/com/javarush/task/task04/task0435/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0435/Solution.java
@@ -7,6 +7,11 @@ package com.javarush.task.task04.task0435;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        for (int i = 1; i <101; i++){
+            if (i%2 == 0) {
+                System.out.println(i);
+            }
+        }
 
     }
 }
--- 1.JavaSyntax/src/com/javarush/task/task04/task0440/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0440/Solution.java
@@ -7,6 +7,9 @@ package com.javarush.task.task04.task0440;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        for (int i = 1; i<101; i++) {
+            System.out.println("Я никогда не буду работать за копейки. Амиго");
+        }
 
     }
 }
