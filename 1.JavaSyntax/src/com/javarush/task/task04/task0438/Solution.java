210ed9f57fedc769d9e1b2b8c1749c586502d44e
TolReed <tolreed@gmail.com>
2019-07-16 19:23:07 +0300

    3 new tasks, the last 3 are left now

--- 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
@@ -7,6 +7,13 @@ package com.javarush.task.task04.task0438;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        for (int i = 1; i <10; i++) {
+            System.out.print("8");
+        }
+
+        for (int j = 0; j <=10; j++) {
+            System.out.println("8");
+        }
 
     }
 }
--- 1.JavaSyntax/src/com/javarush/task/task04/task0439/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0439/Solution.java
@@ -10,6 +10,12 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String n = reader.readLine();
+
+        for (int i = 1; i <=10; i++) {
+            System.out.println(n + " любит меня.");
+        }
 
     }
 }
