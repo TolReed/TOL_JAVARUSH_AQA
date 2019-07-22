30d744d02c2de3300bf9c6528442baedd7853980
TolReed <tolreed@gmail.com>
2019-07-22 19:44:10 +0300

    5.27

--- 1.JavaSyntax/src/com/javarush/task/task05/task0526/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0526/Solution.java
@@ -7,7 +7,39 @@ package com.javarush.task.task05.task0526;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        Man man1 = new Man("Tol", 31, "NY City");
+        Man man2 = new Man("TolRock", 31, "NY City");
+        System.out.println(man1.name + " " + man1.age + " " + man1.address);
+        System.out.println(man2.name + " " + man2.age + " " + man2.address);
+        Woman woman1 = new Woman("Mariamna", 33, "NY City");
+        Woman woman2 = new Woman("Anna", 33, "NY City");
+        System.out.println(woman1.name + " " + woman1.age + " " + woman1.address);
+        System.out.println(woman2.name + " " + woman2.age + " " + woman2.address);
     }
 
     //напишите тут ваш код
+    public static class Man {
+        String name;
+        int age;
+        String address;
+
+        public Man (String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+
+    public static class Woman {
+        String name;
+        int age;
+        String address;
+
+        public Woman (String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+
 }
--- 1.JavaSyntax/src/com/javarush/task/task05/task0529/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0529/Solution.java
@@ -10,5 +10,23 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int count = 0;
+        int sum = 0;
+        while (true)
+        {
+            String s = buffer.readLine();
+            if (s.equals("сумма"))
+                break;
+            int Sn = Integer.parseInt(s);
+
+
+            sum = sum + Sn;
+            count++;
+        }
+        System.out.println(sum);
+
     }
 }
