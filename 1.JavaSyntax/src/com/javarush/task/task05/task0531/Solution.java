ca60d386b86e061ba6648cdcad803ce2dbc20024
TolReed <tolreed@gmail.com>
2019-07-24 18:45:00 +0300

    6.01 - 6.02

--- 1.JavaSyntax/src/com/javarush/task/task05/task0531/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0531/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task05.task0531;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /* 
 Совершенствуем функциональность
@@ -13,14 +14,103 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
 
-        int minimum = min(a, b);
+        int minimum = min(a, b, c, d, e);
 
         System.out.println("Minimum = " + minimum);
     }
 
 
-    public static int min(int a, int b) {
-        return a < b ? a : b;
+    public static int min(int a, int b, int c, int d, int e) {
+        int[] array = {a, b, c, d, e};
+        int min = array[0];
+        for(int i = 0; i < array.length; i++) {
+            if (array[i] < min)
+                min = array[i];
+        }
+        return min;
+
+
     }
+
 }
+
+/* _________________________________________________________________________this code works without min method
+
+
+
+    public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
+
+        int min =  min(a, min(b, min(c, min(d,e))));
+
+        System.out.println("Minimum = " + min);
+    }
+
+
+_____________________________________________________________________________also works code below
+
+    public static int min(int a, int b, int c, int d, int e) {
+        int[] array = {a, b, c, d, e};
+        Arrays.sort(array);
+
+        int value = array[0];
+        return value;
+    }
+
+_____________________________________________________________________________also works code below
+
+private void getMinNum(int... nums) {
+        Arrays.stream(nums).min().ifPresent(System.out::println);
+    }
+_____________________________________________________________________________also works code below
+import java.util.Scanner;
+
+public class MinMax
+{
+  public static void main(String[] args)
+  {
+   int i = 1;
+
+   while (i <= 5)
+   {
+    int smallest = 0;
+    int largest = 0;
+
+    System.out.println("Enter number: ");
+    Scanner input = new Scanner(System.in);
+    int number = input.nextInt();
+
+    System.out.println("Amount of numbers entered: " + i);
+
+    System.out.println("Number entered: " + number);
+
+    if (number < min)
+    {
+      min = number;
+    }
+    System.out.println("Smallest # so far: " + min);
+
+    if (number > max)
+    {
+      max = number;
+    }
+    System.out.println("Largest # so far: " + max);
+
+
+    i++;
+
+   }
+ }
+}
+             */
--- 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
@@ -9,10 +9,21 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int maximum = 77;
+        int n = Integer.parseInt(reader.readLine());
+        while (n > 0) { // перевіряємо чи введене число є більше нуля, а потім вводимо стільки чисел як n
+            int maximum = Integer.MIN_VALUE; // Во всех задачах на минимум и максимум не надо объявлять их изначальное значение нулем:
+                                             // либо инициализируй значением первого элемента,
+                                             // либо инициализируй значениями Integer.MAX_VALUE для минимума и Integer.MIN_VALUE для максимума
+            for (int i = 1; i < (n+1); i++) { //
+                int nums = Integer.parseInt(reader.readLine()); // вводимо числа з клавіатури - n чисел
+                if (nums > maximum) {
+                    maximum = nums; // присвоїти значення максимального числа тому, яке введене з клавіатури (якщо від"ємні, то максимальне з від"ємних чисел)
+                }
+            }
+            System.out.println(maximum); // вивести на екран максимальне число
+            break;
 
-        //напишите тут ваш код
+        }
 
-        System.out.println(maximum);
     }
 }
