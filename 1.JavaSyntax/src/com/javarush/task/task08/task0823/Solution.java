72812c054d7ec5fc9186f11c22e6cc713b971984
TolReed <tolreed@gmail.com>
2019-08-14 14:26:09 +0300

    8.23: "тест рест фест" = "Тест Рест Фест"

--- 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
@@ -14,5 +14,19 @@ public class Solution {
         String string = reader.readLine();
 
         //напишите тут ваш код
+        String sWithoutExtraEscapes = string.replaceAll("\\s+"," ").trim();
+        //Создаем новую строку, в которую помещаем введенную строку,
+        // где методом replaceAll() удаляются все лишние пробелы между словами (\\s+ означает любое количество пробелов) и заменяем их на одинарный пробел.
+        // А методом trim() удаляем вероятные пробелы в начале или конце строки.
+        String[] arr = sWithoutExtraEscapes.split(" ");
+        //останется только разбить полученную строку на строковый массив
+
+        for (String s: arr) {
+            String first = s.substring(0, 1); // получить первий символ каждого слова
+            String after = s.substring(1); // получить оставшийся символ
+            String result = first.toUpperCase() + after + " ";
+
+            System.out.print(result);
+        }
     }
 }
