4ab3e7dddc31c926e7834c3fb92cb8ea2160fbcb
TolReed <tolreed@gmail.com>
2019-08-14 16:01:20 +0300

    8.27 - isDateOdd: true / false

--- 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
@@ -1,17 +1,39 @@
 package com.javarush.task.task08.task0827;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 /* 
 Работа с датой
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         System.out.println(isDateOdd("MAY 1 2013"));
     }
 
-    public static boolean isDateOdd(String date) {
-        return true;
+    public static boolean isDateOdd(String date) throws ParseException {
+
+        boolean odd = true;
+
+        Date yearDate = new Date(date);
+        Date yearStartTime = new Date(date);
+        yearStartTime.setDate(1);      // первое число
+        yearStartTime.setMonth(0);     // месяц январь, нумерация для месяцев 0-11
+
+        long msTimeDistance = yearDate.getTime() - yearStartTime.getTime();
+        long msDay = 24 * 60 * 60 * 1000;
+
+        int dayCount = (int) (msTimeDistance/msDay);
+
+        if (dayCount % 2 != 0) {
+            odd = false;
+        }
+
+        return odd;
+
     }
 }
