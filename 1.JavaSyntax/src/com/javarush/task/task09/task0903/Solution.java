3b24a8f207728f24aa44f507ef416142444e4491
TolReed <tolreed@gmail.com>
2019-08-19 19:10:26 +0300

    9.03: Написать пять методов, которые вызывают друг друга. Метод должен вернуть номер строки кода, из которого вызвали этот метод.
    Кто меня вызывал?
    Написать пять методов, которые вызывают друг друга.
    Метод должен вернуть номер строки кода, из которого вызвали этот метод.
    Воспользуйся функцией: element.getLineNumber().

--- 1.JavaSyntax/src/com/javarush/task/task09/task0903/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0903/Solution.java
@@ -11,25 +11,35 @@ public class Solution {
 
     public static int method1() {
         method2();
-        return  /*напишите тут ваш код*/ 0;
+        /*напишите тут ваш код*/
+        StackTraceElement[] SteckTraceElements = Thread.currentThread().getStackTrace();
+        return  SteckTraceElements[2].getLineNumber();
     }
 
     public static int method2() {
         method3();
-        return  /*напишите тут ваш код*/ 0;
+        /*напишите тут ваш код*/
+        StackTraceElement[] SteckTraceElements = Thread.currentThread().getStackTrace();
+        return  SteckTraceElements[2].getLineNumber();
     }
 
     public static int method3() {
         method4();
-        return  /*напишите тут ваш код*/ 0;
+        /*напишите тут ваш код*/
+        StackTraceElement[] SteckTraceElements = Thread.currentThread().getStackTrace();
+        return  SteckTraceElements[2].getLineNumber();
     }
 
     public static int method4() {
         method5();
-        return  /*напишите тут ваш код*/ 0;
+        /*напишите тут ваш код*/
+        StackTraceElement[] SteckTraceElements = Thread.currentThread().getStackTrace();
+        return  SteckTraceElements[2].getLineNumber();
     }
 
     public static int method5() {
-        return  /*напишите тут ваш код*/ 0;
+        /*напишите тут ваш код*/
+        StackTraceElement[] SteckTraceElements = Thread.currentThread().getStackTrace();
+        return  SteckTraceElements[2].getLineNumber();
     }
 }
