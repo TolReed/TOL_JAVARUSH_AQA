19a2f52062bf63339731777ca9e2db96a041c00f
TolReed <tolreed@gmail.com>
2019-08-20 17:26:26 +0300

    9.05: Там, в синих глубинах стек-трейса…
    Там, в синих глубинах стек-трейса…
    Написать метод, который возвращает результат - глубину его стек-трейса - количество методов в нем (количество элементов в списке).
    Это же число метод должен выводить на экран.

--- 1.JavaSyntax/src/com/javarush/task/task09/task0905/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0905/Solution.java
@@ -4,13 +4,16 @@ package com.javarush.task.task09.task0905;
 Там, в синих глубинах стек-трейса…
 */
 
-public class Solution {/*
+public class Solution {
     public static void main(String[] args) {
         int deep = getStackTraceDeep();
     }
 
     public static int getStackTraceDeep() {
         //напишите тут ваш код
-    }*/
+        StackTraceElement[] StackTraceElements = Thread.currentThread().getStackTrace();
+        System.out.println(StackTraceElements.length);
+        return StackTraceElements.length;
+    }
 }
 
