17688fbf0426b01ec40af5aa5e2633c858485458
TolReed <tolreed@gmail.com>
2019-08-21 17:45:17 +0300

    9.12: Исключение при работе с числами
    Исключение при работе с числами
    Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода:
    int num=Integer.parseInt("XYZ");
    System.out.println(num);

--- 1.JavaSyntax/src/com/javarush/task/task09/task0912/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0912/Solution.java
@@ -7,9 +7,14 @@ package com.javarush.task.task09.task0912;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        try {
+            int num = Integer.parseInt("XYZ");
+            System.out.println(num);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
 
-        int num = Integer.parseInt("XYZ");
-        System.out.println(num);
+
 
         //напишите тут ваш код
     }
