1c572c63d9019f39317621ca7801789fe878c7a1
TolReed <tolreed@gmail.com>
2019-08-21 17:34:20 +0300

    9.08: Исключение при работе со строками
    Исключение при работе со строками
    
    Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода:
    String s = null;
    String m = s.toLowerCase();

--- 1.JavaSyntax/src/com/javarush/task/task09/task0908/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0908/Solution.java
@@ -7,9 +7,13 @@ package com.javarush.task.task09.task0908;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        try {
+            String s = null;
+            String m = s.toLowerCase();
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
 
-        String s = null;
-        String m = s.toLowerCase();
 
         //напишите тут ваш код
     }
