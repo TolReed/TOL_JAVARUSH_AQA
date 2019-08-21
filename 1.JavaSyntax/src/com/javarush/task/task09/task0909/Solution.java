9cd8e49735096cd8f666560b944f11192d472cde
TolReed <tolreed@gmail.com>
2019-08-21 17:38:24 +0300

    9.09: Исключение при работе с массивами
    Исключение при работе с массивами
    
    Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода:
    int[] m = new int[2];
    m[8] = 5;

--- 1.JavaSyntax/src/com/javarush/task/task09/task0909/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0909/Solution.java
@@ -7,9 +7,14 @@ package com.javarush.task.task09.task0909;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        try {
+            int[] m = new int[2];
+            m[8] = 5;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+        }
 
-        int[] m = new int[2];
-        m[8] = 5;
+
 
         //напишите тут ваш код
     }
