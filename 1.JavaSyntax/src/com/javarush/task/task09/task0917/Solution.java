31c4529f4908e7a96f9c9d2263a23148d276697f
TolReed <tolreed@gmail.com>
2019-08-22 15:39:58 +0300

    9.17: Перехват unchecked-исключений
    Перехват unchecked-исключений
    В методе handleExceptions обработайте все unchecked исключения.
    Нужно вывести стек-трейс каждого возникшего исключения используя метод printStack.
    Можно использовать только один блок try..catch

--- 1.JavaSyntax/src/com/javarush/task/task09/task0917/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0917/Solution.java
@@ -10,9 +10,18 @@ public class Solution {
     }
 
     public static void handleExceptions(Solution obj) {
-        obj.method1();
-        obj.method2();
-        obj.method3();
+        try {
+            obj.method1();
+            obj.method2();
+            obj.method3();
+        } catch (NullPointerException e) {
+            printStack(e);
+        } catch (IndexOutOfBoundsException e) {
+            printStack(e);
+        } catch (NumberFormatException e) {
+            printStack(e);
+        }
+
     }
 
     public static void printStack(Throwable throwable) {
