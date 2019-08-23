98923185c322f864211f2462a97e480854750dc2
TolReed <tolreed@gmail.com>
2019-08-23 14:29:37 +0300

    9.30: A great task - sort numbers and words > Задача по алгоритмам
    Задача по алгоритмам
    Задача: Пользователь вводит с клавиатуры список слов (и чисел).
    Слова вывести в возрастающем порядке, числа - в убывающем.

--- 1.JavaSyntax/src/com/javarush/task/task09/task0930/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0930/Solution.java
@@ -30,6 +30,23 @@ public class Solution {
 
     public static void sort(String[] array) {
         // напишите тут ваш код
+        for (int i = 0;  i < array.length; i++) { // проходимося по всьому масиву слів і чисел
+            for (int j = i; j < array.length; j++) { // проходимося по всьому масиву слів і чисел
+                if (isNumber(array[j]) && isNumber(array[i])) { //сортуємо числа, перевіряємо чи елемент є числом
+                    if (Integer.parseInt(array[j]) > Integer.parseInt(array[i])) { //зрівнюємо числа для того щоб вивести у спадаючому порядку
+                        String a = array[j];
+                        array[j] = array[i];
+                        array[i] = a;
+                    }
+                } else if (!(isNumber(array[j])) && !(isNumber(array[i]))) { //сортуємо слова, перевіряємо чи елемент !не є числом
+                    if (isGreaterThan(array[i], array[j])) { //зрівнюємо слова для того аби вивести у зростаючому порядку
+                        String a = array[j];
+                        array[j] = array[i];
+                        array[i] = a;
+                    }
+                }
+            }
+        }
     }
 
     // Метод для сравнения строк: 'а' больше чем 'b'
