ead324eeb06c3042e6d984123c20c4083ef0896b
TolReed <tolreed@gmail.com>
2019-08-23 13:02:05 +0300

    9.26: Список из массивов чисел: int[] array = new int[n];
    Список из массивов чисел
    Создать список, элементами которого будут массивы чисел.
    Добавить в список пять объектов-массивов длиной 5, 2, 4, 7, 0 соответственно.
    Заполнить массивы любыми данными и вывести их на экран.
    
    int[] array1 = new int[5];
    int[] array2 = new int[2];
    int[] array3 = new int[4];
    int[] array4 = new int[7];
    int[] array5 = new int[0];

--- 1.JavaSyntax/src/com/javarush/task/task09/task0926/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0926/Solution.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 Список из массивов чисел
 */
 
-public class Solution {/*
+public class Solution {
     public static void main(String[] args) {
         ArrayList<int[]> list = createList();
         printList(list);
@@ -14,7 +14,40 @@ public class Solution {/*
 
     public static ArrayList<int[]> createList() {
         //напишите тут ваш код
-        return createList();
+        ArrayList<int[]> array = new ArrayList<>();
+        //Добавить в список пять объектов-массивов длиной 5, 2, 4, 7, 0
+        int[] array1 = new int[5];
+        int[] array2 = new int[2];
+        int[] array3 = new int[4];
+        int[] array4 = new int[7];
+        int[] array5 = new int[0];
+        //Добавить в список пять объектов-массивов длиной 5, 2, 4, 7, 0
+
+        //Заполнить массивы любыми данными и вывести их на экран
+        for (int i = 0; i < 5; i++) {
+            array1[i] = i;
+        }
+
+        for (int i = 0; i < 2; i++) {
+            array2[i] = i;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            array3[i] = i;
+        }
+
+        for (int i = 0; i < 7; i++) {
+            array4[i] = i;
+        }
+
+        array.add(0, array1);
+        array.add(array2);
+        array.add(array3);
+        array.add(array4);
+        array.add(array5);
+
+        return array;
+        //Заполнить массивы любыми данными и вывести их на экран
     }
 
     public static void printList(ArrayList<int[]> list) {
@@ -23,5 +56,5 @@ public class Solution {/*
                 System.out.println(x);
             }
         }
-    }*/
+    }
 }
