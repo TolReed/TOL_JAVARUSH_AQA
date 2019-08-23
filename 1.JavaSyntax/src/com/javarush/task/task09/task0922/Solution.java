728c76ddf7ec0abcf745af548a25dfadc73cc5f5
TolReed <tolreed@gmail.com>
2019-08-23 11:29:42 +0300

    9,22: Гарно - Какое сегодня число?
    Какое сегодня число?
    Ввести с клавиатуры дату в формате "2013-08-18"
    Вывести на экран введенную дату в виде "AUG 18, 2013".
    Воспользоваться объектом Date и SimpleDateFormat.

--- 1.JavaSyntax/src/com/javarush/task/task09/task0922/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0922/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task09.task0922;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -14,5 +15,12 @@ public class Solution {
 
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); //ініціалізуємо буферрідер
+        SimpleDateFormat SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH); //створюємо змінну формату дати
+        SimpleDateFormat SimpleDateFormatNew = new SimpleDateFormat("MMM d, yyyy", Locale.ENGLISH); //створюємо нову змінну формату дати
+        Date date = SimpleDateFormat.parse(reader.readLine()); //читаємо введену дату з клавуатури
+
+        System.out.println(SimpleDateFormatNew.format(date).toUpperCase()); //приводимо введену дату до формату і виводимо на екран
+
     }
 }
