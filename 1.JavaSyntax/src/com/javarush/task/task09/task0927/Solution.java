5b5af6391eace4f31db433584f60c7631aa5c45a
TolReed <tolreed@gmail.com>
2019-08-23 12:48:56 +0300

    9.27; Десять котов - Map<String, Cat>; HashSet<Cat> = map.values();
    Десять котов
    Есть класс кот - Cat, с полем "имя" (String).
    Создать словарь Map<String, Cat> и добавить туда 10 котов в виде "Имя"-"Кот".
    Получить из Map множество(Set) всех котов и вывести его на экран.

--- 1.JavaSyntax/src/com/javarush/task/task09/task0927/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0927/Solution.java
@@ -9,7 +9,7 @@ import java.util.Set;
 Десять котов
 */
 
-public class Solution {/*
+public class Solution {
     public static void main(String[] args) {
         Map<String, Cat> map = createMap();
         Set<Cat> set = convertMapToSet(map);
@@ -18,10 +18,26 @@ public class Solution {/*
 
     public static Map<String, Cat> createMap() {
         //напишите тут ваш код
+        Map<String, Cat> catMap = new HashMap<>(); //Создать словарь Map<String, Cat>
+                                                   // и добавить туда 10 котов в виде "Имя"-"Кот".
+        catMap.put("1", new Cat("Alpha"));
+        catMap.put("2", new Cat("Bravo"));
+        catMap.put("3", new Cat("Celta"));
+        catMap.put("4", new Cat("Lucky"));
+        catMap.put("5", new Cat("Omega"));
+        catMap.put("6", new Cat("Crabsik"));
+        catMap.put("7", new Cat("Elephantik"));
+        catMap.put("8", new Cat("Hooray"));
+        catMap.put("9", new Cat("Woooooow"));
+        catMap.put("10", new Cat("Future"));
+
+        return catMap;
     }
 
     public static Set<Cat> convertMapToSet(Map<String, Cat> map) {
         //напишите тут ваш код
+        HashSet<Cat> catSet = new HashSet<Cat>(map.values()); //Получить из Map множество(Set) всех котов и вывести его на экран.
+        return catSet;
     }
 
     public static void printCatSet(Set<Cat> set) {
@@ -40,7 +56,7 @@ public class Solution {/*
         public String toString() {
             return "Cat " + this.name;
         }
-    }*/
+    }
 
 
 }
