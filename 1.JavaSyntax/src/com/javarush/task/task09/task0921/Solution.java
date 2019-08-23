c36e255b26b89054985103124d5ee03b1f848e46
TolReed <tolreed@gmail.com>
2019-08-23 11:22:29 +0300

    9.23: Дуже крута задача - Гласные и согласные
    Гласные и согласные
    Написать программу, которая вводит с клавиатуры строку текста.
    Программа должна вывести на экран две строки:
    1. первая строка содержит только гласные буквы из введённой строки.
    2. вторая - только согласные буквы и знаки препинания из введённой строки.
    Буквы соединять пробелом, каждая строка должна заканчиваться пробелом.
    Пример ввода:
    Мама мыла раму.
    
    Пример вывода:
    а а ы а а у
    М м м л р м .

--- 1.JavaSyntax/src/com/javarush/task/task09/task0921/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0921/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task09.task0921;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,5 +17,27 @@ public class Solution {
 
     public static void readData() {
         //напишите тут ваш код
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            while (true) {
+                numbers.add(Integer.parseInt(reader.readLine()));
+                }
+            } catch (Exception e) {
+            e.printStackTrace();
+
+            for (int i: numbers) {
+                System.out.println(i);
+            }
+        }
+
     }
+
 }
+/*
+future improvements:
+1.  Так как list никак не чистится, то если в main запустить readData() несколько раз - каждый раз list будет увеличиваться,
+    и выводить результаты еще и предыдущих вызовов метода readData().
+    Лучше сделать его локальной переменной метода.
+2.  Change 101 in order to input was much as I want numbers and add them to the array
+*/
--- 1.JavaSyntax/src/com/javarush/task/task09/task0923/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0923/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task09.task0923;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /* 
 Гласные и согласные
@@ -12,6 +13,30 @@ public class Solution {
 
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        ArrayList<Character> vovels = new ArrayList<>(); // голосні
+        ArrayList<Character> consostants = new ArrayList<>(); // приголосні
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); //ініціалізуємо буфферрідер
+        String textLine = reader.readLine(); //зчитуємо з клавіатури
+
+        for (int i = 0; i < textLine.length(); i ++) { //проходимося в циклі по введеному реченню або слову
+            char currentChar = textLine.charAt(i); //ініціалізуємо значення поточного символа з прочитаного слова або речення
+
+            if (isVowel(currentChar)) { //робимо перевірку чи є поточний символ такий як у масиві голосних
+                vovels.add(currentChar); //у разі ТРУ - додаємо його в масив голосних
+            } else if (currentChar != ' ') { //в іншому випадку або якщо поточний символ не дорівнює пробіл ...
+                consostants.add(currentChar); //... - додаємо його до масиву приголосних
+            }
+        }
+
+        for (char vovel: vovels) { //виводимо на екран голосні
+            System.out.print(vovel + " ");
+        }
+
+        System.out.println(); //переводимо на новий рядочок, щоб вивести туди приголосні - відповідно до завдання
+
+        for (char consostant: consostants) { //виводимо на екран приголосні
+            System.out.print(consostant + " ");
+        }
     }
 
     // метод проверяет, гласная ли буква
