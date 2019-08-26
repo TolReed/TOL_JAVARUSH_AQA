4b86e65ccf7d5a7cc0c28e3dbcc566f56a2b9519
TolReed <tolreed@gmail.com>
2019-08-26 14:38:38 +0300

    6.18: Kis();

--- 1.JavaSyntax/src/com/javarush/task/task09/task0929/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0929/Solution.java
@@ -10,10 +10,19 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String sourceFileName = reader.readLine();
-        String destinationFileName = reader.readLine();
+        InputStream fileInputStream;
 
-        InputStream fileInputStream = getInputStream(sourceFileName);
+        try {
+            String sourceFileName = reader.readLine();
+            fileInputStream = getInputStream(sourceFileName);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не существует.");
+            String sourceFileName = reader.readLine();
+            fileInputStream = getInputStream(sourceFileName);
+        }
+
+        String destinationFileName = reader.readLine();
         OutputStream fileOutputStream = getOutputStream(destinationFileName);
 
         while (fileInputStream.available() > 0) {
@@ -23,7 +32,10 @@ public class Solution {
 
         fileInputStream.close();
         fileOutputStream.close();
-    }
+
+        }
+
+
 
     public static InputStream getInputStream(String fileName) throws IOException {
         return new FileInputStream(fileName);
