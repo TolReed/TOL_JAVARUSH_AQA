2c407ff15ada9e0ae10bb825afa5afc8234b649a
TolReed <tolreed@gmail.com>
2019-10-01 19:38:42 +0300

    10.11 - print code 40 times and remove first letter for the next line - SUBSTRING method

--- 1.JavaSyntax/src/com/javarush/task/task10/task1011/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1011/Solution.java
@@ -7,9 +7,14 @@ package com.javarush.task.task10.task1011;
 public class Solution {
     public static void main(String[] args) {
         String s = "Я не хочу изучать Java, я хочу большую зарплату";
+        System.out.println(s);
 
+
+        for (int i = 1; i < 40; i++) {
+
+
+            System.out.println(s.substring(i));
+        }
         //напишите тут ваш код
     }
-
 }
-
