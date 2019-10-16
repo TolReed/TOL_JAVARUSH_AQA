c250ae88bb39aea49f3763a5e111ff6a4110d4a8
TolReed <tolreed@gmail.com>
2019-10-16 14:38:55 +0300

    10.13 - Конструкторы класса Human
    Требования:
    1. Программа не должна считывать данные с клавиатуры.
    2. В классе Human должно быть 6 полей.
    3. Все поля класса Human должны быть private.
    4. В классе Human должно быть 10 конструкторов.
    5. Все конструкторы класса Human должны быть public.

--- 1.JavaSyntax/src/com/javarush/task/task10/task1013/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1013/Solution.java
@@ -10,12 +10,76 @@ public class Solution {
 
     public static class Human {
         // Напишите тут ваши переменные и конструкторы
-        String firstName;
-        String secondName;
-        int age;
-        Boolean sex;
-        String address;
-        int phoneNumber;
+        private String firstName;
+        private String secondName;
+        private int age;
+        private Boolean sex;
+        private String address;
+        private int phoneNumber;
 
+
+        public Human () {
+        }
+
+        public Human (String firstName) {
+            this.firstName = firstName;
+        }
+
+        public Human (String firstName, String secondName) {
+            this.firstName = firstName;
+            this.secondName = secondName;
+        }
+
+        public Human (String firstName, String secondName, int age) {
+            this.firstName = firstName;
+            this.secondName = secondName;
+            this.age = age;
+        }
+
+        public Human (String firstName, String secondName, int age, Boolean sex) {
+            this.firstName = firstName;
+            this.secondName = secondName;
+            this.age = age;
+            this.sex = sex;
+        }
+
+        public Human (String firstName, String secondName, int age, Boolean sex, String address) {
+            this.firstName = firstName;
+            this.secondName = secondName;
+            this.age = age;
+            this.sex = sex;
+            this.address = address;
+        }
+
+        public Human (String firstName, String secondName, int age, Boolean sex, String address, int phoneNumber) {
+            this.firstName = firstName;
+            this.secondName = secondName;
+            this.age = age;
+            this.sex = sex;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public Human (String address, int phoneNumber) {
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public Human (int age, Boolean sex, String address, int phoneNumber) {
+            this.age = age;
+            this.sex = sex;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public Human (String firstName, String secondName, Boolean sex, String address, int phoneNumber) {
+            this.firstName = firstName;
+            this.secondName = secondName;
+            this.sex = sex;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+        }
     }
+
+
 }
