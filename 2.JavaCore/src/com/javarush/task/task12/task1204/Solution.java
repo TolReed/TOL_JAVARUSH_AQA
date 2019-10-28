d9b04ea14d3ca1a26b2f08c6bc129d016703a8e5
TolReed <tolreed@gmail.com>
2019-10-28 17:37:01 +0200

    12.04 - if (o instanceof Cat) { System.out.println("Кошка"); }
    То ли птица, то ли лампа
    Написать метод, который определяет, объект какого класса ему передали, и выводит на экран одну из надписей: Кошка, Собака, Птица, Лампа.

--- 2.JavaCore/src/com/javarush/task/task12/task1204/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1204/Solution.java
@@ -4,6 +4,9 @@ package com.javarush.task.task12.task1204;
 То ли птица, то ли лампа
 */
 
+
+
+
 public class Solution {
     public static void main(String[] args) {
         printObjectType(new Cat());
@@ -15,6 +18,19 @@ public class Solution {
 
     public static void printObjectType(Object o) {
         //Напишите тут ваше решение
+        if (o instanceof Cat) {
+            System.out.println("Кошка");
+        }
+        if (o instanceof Dog) {
+            System.out.println("Собака");
+        }
+        if (o instanceof Bird) {
+            System.out.println("Птица");
+        }
+        if (o instanceof Lamp) {
+            System.out.println("Лампа");
+        }
+
     }
 
     public static class Cat {
@@ -29,3 +45,14 @@ public class Solution {
     public static class Lamp {
     }
 }
+
+/*
+Теперь по сути: так как в метод printObjectType не передаётся какой-то определённый тип,
+узнать этот тип без всяких if-else можно только с помощью рефлексии. Выглядит это как-то так:
+
+System.out.println(o.getClass().getMethod("toString").invoke(o));
+
+
+вставляешь этот код в метод printObjectType и он будет превращать в строку и выводить в консоль обьект любого класса,
+который ты туда передашь. А toString переопределён нормально, его задача  - просто возвращать строку.
+* */
