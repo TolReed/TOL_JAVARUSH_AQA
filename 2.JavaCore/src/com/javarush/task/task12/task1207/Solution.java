bf82604b2e7fde75e58a2d5a767f76e7821f9c2f
TolReed <tolreed@gmail.com>
2019-10-29 21:14:19 +0200

    12.07 - Int и Integer Написать два метода: print(int) и print(Integer). Написать такой код в методе main, чтобы вызвались они оба.

--- 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -6,8 +6,17 @@ Int и Integer
 
 public class Solution {
     public static void main(String[] args) {
+        print(5);
+        Integer z = new Integer(7); print(z);
 
     }
 
     //Напишите тут ваши методы
+    public static void print(int a) {
+
+    }
+
+    public static void print(Integer a) {
+
+    }
 }
