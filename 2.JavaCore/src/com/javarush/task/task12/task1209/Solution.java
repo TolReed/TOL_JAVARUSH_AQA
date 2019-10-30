abea3d775dcb814ece0b9a4733cc1dd3492fc020
TolReed <tolreed@gmail.com>
2019-10-30 19:30:43 +0200

    12.09 - Три метода и минимум Написать public static методы: int min(int, int), long min(long, long), double min(double, double). Каждый метод должен возвращать минимальное из двух переданных в него чисел.

--- 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
@@ -10,4 +10,21 @@ public class Solution {
     }
 
     //Напишите тут ваши методы
+    public static int min(int a, int b) {
+        if (a > b) { return b; }
+        else if (a < b) { return a; }
+        else { return b; }
+
+    }
+    public static long min(long a, long b) {
+        if (a > b) { return b; }
+        else if (a < b) { return a; }
+        else { return b; }
+
+    }
+    public static double min(double a, double b) {
+        if (a > b) { return b; }
+        else if (a < b) { return a; }
+        else { return b; }
+    }
 }
