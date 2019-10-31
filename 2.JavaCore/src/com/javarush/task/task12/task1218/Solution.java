90aa1905c435ceb5be8a12dd538f42b74ba63bdb
TolReed <tolreed@gmail.com>
2019-10-31 18:06:38 +0200

    12.18 - implements - Есть, летать и двигаться Есть public интерфейсы CanFly(летать), CanMove(передвигаться), CanEat(есть). Добавь эти интерфейсы классам Dog(собака), Car(автомобиль), Duck(утка), Airplane(самолет).

--- 2.JavaCore/src/com/javarush/task/task12/task1218/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1218/Solution.java
@@ -21,15 +21,51 @@ public class Solution {
         public void eat();
     }
 
-    public class Dog {
+    public class Dog implements CanEat, CanMove {
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void eat() {
+
+        }
     }
 
-    public class Duck {
+    public class Duck implements CanEat, CanMove, CanFly {
+        @Override
+        public void fly() {
+
+        }
+
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void eat() {
+
+        }
     }
 
-    public class Car {
+    public class Car implements CanMove {
+        @Override
+        public void move() {
+
+        }
     }
 
-    public class Airplane {
+    public class Airplane implements CanFly, CanMove {
+        @Override
+        public void fly() {
+
+        }
+
+        @Override
+        public void move() {
+
+        }
     }
 }
