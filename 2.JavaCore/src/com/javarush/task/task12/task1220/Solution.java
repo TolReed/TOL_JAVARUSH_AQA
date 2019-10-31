f959bb6a7d20baba143b6b7f433b46a705815abb
TolReed <tolreed@gmail.com>
2019-10-31 18:14:53 +0200

    12.20 - public abstract class Human implements
    Класс Human и интерфейсы CanRun, CanSwim
    Напиши public класс Human(человек) и public интерфейсы CanRun(бежать/ездить), CanSwim(плавать).
    Добавь в каждый интерфейс по oдному методу.
    Добавь эти интерфейсы классу Human, но не реализуй методы.
    Объяви класс Human абстрактным.

--- 2.JavaCore/src/com/javarush/task/task12/task1220/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1220/Solution.java
@@ -9,5 +9,16 @@ public class Solution {
 
     }
 
-    //add public interfaces and a public class here - добавь public интерфейсы и public класс тут
+    public interface CanRun {
+        public void run();
+    }
+
+    public interface CanSwim {
+        public void swim();
+    }
+
+    public abstract class Human implements CanRun, CanSwim {
+
+    }
+
 }
