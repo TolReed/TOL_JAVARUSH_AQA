04b74b2ccbf92cbe3c9c5d223912535dce6f648f
TolReed <tolreed@gmail.com>
2019-11-04 18:30:35 +0200

    12.27 - CanFly, CanRun, CanSwim для классов Duck, Penguin, Toad Есть интерфейсы CanFly(летать), CanSwim(плавать), CanRun(бегать). Добавь эти интерфейсы классам Duck(утка), Penguin(пингвин), Toad(жаба)

--- 2.JavaCore/src/com/javarush/task/task12/task1227/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1227/Solution.java
@@ -21,15 +21,42 @@ public class Solution {
         public void swim();
     }
 
-    public class Duck {
+    public class Duck implements CanSwim, CanFly, CanRun {
 
+        @Override
+        public void swim() {
+
+        }
+
+        @Override
+        public void fly() {
+
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 
-    public class Penguin {
+    public class Penguin implements CanSwim, CanRun{
 
+        @Override
+        public void run() {
+
+        }
+
+        @Override
+        public void swim() {
+
+        }
     }
 
-    public class Toad {
+    public class Toad implements CanSwim {
 
+        @Override
+        public void swim() {
+
+        }
     }
 }
