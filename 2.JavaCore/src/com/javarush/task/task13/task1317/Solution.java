5786290ea779fe6c1bf1833602758e0e4160f654
TolReed <tolreed@gmail.com>
2019-11-15 15:42:14 +0200

    13.17 - The weather is fine - Today, iнтерфейс Weather. параметр type с методом getWeatherType(). Iнтерфейсы Weather i WeatherType уже реализованi

--- 2.JavaCore/src/com/javarush/task/task13/task1317/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1317/Solution.java
@@ -4,14 +4,14 @@ package com.javarush.task.task13.task1317;
 The weather is fine
 */
 
-public class Solution {/*
+public class Solution {
     public static void main(String[] args) {
         System.out.println(new Today(WeatherType.CLOUDY));
         System.out.println(new Today(WeatherType.FOGGY));
         System.out.println(new Today(WeatherType.FREEZING));
     }
 
-    static class Today {
+    static class Today implements Weather {
         private String type;
 
         Today(String type) {
@@ -22,5 +22,10 @@ public class Solution {/*
         public String toString() {
             return String.format("Today it will be %s", this.getWeatherType());
         }
-    }*/
+
+        @Override
+        public String getWeatherType() {
+            return this.type;
+        }
+    }
 }
