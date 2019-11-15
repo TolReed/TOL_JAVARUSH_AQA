06955a95b0b57f2480d34ff996173b12abb246aa
TolReed <tolreed@gmail.com>
2019-11-15 15:51:29 +0200

    13.22 - StringObject реализуй интерфейс SimpleObject с параметром типа String.

--- 2.JavaCore/src/com/javarush/task/task13/task1322/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1322/Solution.java
@@ -4,13 +4,21 @@ package com.javarush.task.task13.task1322;
 Интерфейс SimpleObject
 */
 
-public class Solution {/*
+public class Solution {
     public static void main(String[] args) throws Exception {
-        SimpleObject<String> stringObject = new StringObject<Object>();
+        SimpleObject<String> stringObject = new StringObject<String>();
     }
 
     interface SimpleObject<T> {
         SimpleObject<T> getInstance();
-    }*/
+    }
+
+    public static class StringObject<S> implements SimpleObject <String> {
+
+        @Override
+        public SimpleObject<String> getInstance() {
+            return null;
+        }
+    }
 
 }
