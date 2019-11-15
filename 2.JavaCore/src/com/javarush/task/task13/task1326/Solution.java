b5eeb9219c6b4eb4056f5b1e8d667e8a4b52b9bb
TolReed <tolreed@gmail.com>
2019-11-15 16:43:05 +0200

    13.26 - wow - sort numbers from file > how to read numbers and strings
    Сортировка четных чисел из файла
    1. Ввести имя файла с консоли.
    2. Прочитать из него набор чисел.
    3. Вывести на консоль только четные, отсортированные по возрастанию.

--- 2.JavaCore/src/com/javarush/task/task13/task1326/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1326/Solution.java
@@ -4,8 +4,80 @@ package com.javarush.task.task13.task1326;
 Сортировка четных чисел из файла
 */
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // напишите тут ваш код
+        // Строки и числа мы читаем так:
+            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+            String fileName = consoleReader.readLine();
+
+            List<Integer> numbers = new ArrayList<>();
+            FileInputStream inputStream = new FileInputStream(fileName);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String str;
+            while ((str = reader.readLine()) != null) {
+                int number = Integer.parseInt(str);
+                numbers.add(number);
+            }
+
+            Collections.sort(numbers);
+
+            for(int i =0; i<numbers.size(); i++){
+                if (numbers.get(i)%2==0){
+                    System.out.println(numbers.get(i));
+                }
+        }
+
+            reader.close();
+            consoleReader.close();
+        }
+    }
+
+
+
+/*
+Запиши себе в тетрадку.
+Если мы хотим считать байты. Либо это копирование файла или работа с байтами, мы считываем байты через метод read(),
+который возвращает значение 0..255. В это значение влезет английская раскладка, цифры и спецзнаки.
+Но твой код к примеру, файл
+-34
+100
+прочитает как:
+45
+51
+52
+10
+49
+48
+48
+То есть шесть байт, первый байт, это знак минус, потом байты отвечающие за 3, и за 4,
+потом 10 - это байт перевода строки, потом 49 это байт отвечающий за единицу, и два ноля.
+
+Байты мы читаем:
+
+public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+
+        List<Integer> list = new ArrayList<>();
+        FileInputStream inputStream = new FileInputStream(fileName);
+
+        while (inputStream.available() > 0) {
+            int b = inputStream.read();
+            list.add(b);
+        }
+
+        reader.close();
     }
 }
+
+*/
