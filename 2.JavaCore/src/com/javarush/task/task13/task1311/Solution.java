c0afe42ba1f9249d4faa42e4c545222cf268ca8c
TolReed <tolreed@gmail.com>
2019-11-15 15:36:19 +0200

    13.11 - English Translator

--- 2.JavaCore/src/com/javarush/task/task13/task1311/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1311/Solution.java
@@ -4,10 +4,18 @@ package com.javarush.task.task13.task1311;
 Нанимаем переводчика
 */
 
-public class Solution {/*
+public class Solution {
     public static void main(String[] args) throws Exception {
         EnglishTranslator englishTranslator = new EnglishTranslator();
         System.out.println(englishTranslator.translate());
+    }
+
+    public static class EnglishTranslator extends Translator {
+
+        @Override
+        public String getLanguage() {
+            return "английского";
+        }
     }
 
     public static abstract class Translator {
@@ -16,6 +24,6 @@ public class Solution {/*
         public String translate() {
             return "Я переводчик с " + getLanguage();
         }
-    }*/
+    }
 
 }
