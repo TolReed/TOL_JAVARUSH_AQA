25b2a444cfb9e5e1f367b36298fde96ab21fe510
TolReed <tolreed@gmail.com>
2019-11-22 15:49:18 +0200

    13.19 - wow - read from console, InputStreamReader с BufferedReader, FileWritter

--- 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -1,16 +1,47 @@
 package com.javarush.task.task13.task1319;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /* 
 Писатель в файл с консоли
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // напишите тут ваш код
+
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = consoleReader.readLine();
+        FileInputStream inputStream = new FileInputStream(fileName);
+
+        try {
+
+            InputStreamReader inputStreamReader = new InputStreamReader(System.in); // поток чтения с консоли
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader); // соединяем InputStreamReader с BufferedReader
+
+            File file = new File("/Users/anatolii.dudochkin/Desktop/Tol/tol.txt");
+            FileWriter fileWriter = new FileWriter(file); // поток, который подключается к текстовому файлу
+            BufferedWriter bufferWritter = new BufferedWriter(fileWriter); // соединяем FileWriter с BufferedWriter
+
+
+            while (true) {
+                String line = bufferedReader.readLine();
+                bufferWritter.write(line);
+                bufferWritter.newLine();
+
+                if (line.equals("exit")) {
+                    break;
+                }
+
+            }
+
+            bufferedReader.close();
+            bufferWritter.close();
+
+
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+
     }
 }
