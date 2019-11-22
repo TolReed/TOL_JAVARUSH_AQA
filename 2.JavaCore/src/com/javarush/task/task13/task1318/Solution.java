841a55dd6108559032fdc8f2203f59f035f8f2cb
TolReed <tolreed@gmail.com>
2019-11-22 14:26:14 +0200

    13.18 - read a file from console,  and write on screen

--- 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task13.task1318;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.io.FileInputStream;
 
 /* 
@@ -10,7 +13,23 @@ import java.io.FileInputStream;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // напишите тут ваш код
+        // Строки и числа мы читаем так:
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = consoleReader.readLine();
+        FileInputStream inputStream = new FileInputStream(fileName);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+
+        while ( inputStream.available() > 0) {
+            System.out.print((char)inputStream.read()); //inputStream.read() читает не строки, а отдельные байты,
+                                                    // т.е. ты каждый байт выводишь в виде новой строки
+        }
+
+        reader.close();
+        consoleReader.close();
+        inputStream.close();
     }
 }
