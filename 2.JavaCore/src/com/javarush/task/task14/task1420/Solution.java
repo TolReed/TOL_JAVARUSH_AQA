8d6fa123964d061b865afacccbec51905799569b
TolReed <tolreed@gmail.com>
2019-12-03 19:38:54 +0200

    tol

--- 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -7,6 +7,6 @@ package com.javarush.task.task14.task1420;
 public class Solution {
     public static void main(String[] args) throws Exception {
 
-        String toDoTT = "NOD Tol";
+        String toDoTT = "NOD Tol 777";
     }
 }
