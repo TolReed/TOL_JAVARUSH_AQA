c46437a0e2de5e0217f48750a378888353ed5304
TolReed <tolreed@gmail.com>
2020-01-02 15:47:15 +0200

    14.08 - Hen - wow

--- 2.JavaCore/src/com/javarush/task/task14/task1408/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1408/Solution.java
@@ -4,20 +4,39 @@ package com.javarush.task.task14.task1408;
 Куриная фабрика
 */
 
-public class Solution {/*
+public class Solution {
     public static void main(String[] args) {
         Hen hen = HenFactory.getHen(Country.BELARUS);
         hen.getCountOfEggsPerMonth();
+        System.out.println(hen.getDescription());
+        Hen hen1 = HenFactory.getHen(Country.MOLDOVA);
+        hen1.getCountOfEggsPerMonth();
+        System.out.println(hen1.getDescription());
+        Hen hen2 = HenFactory.getHen(Country.UKRAINE);
+        hen2.getCountOfEggsPerMonth();
+        System.out.println(hen2.getDescription());
+        Hen hen3 = HenFactory.getHen(Country.RUSSIA);
+        hen3.getCountOfEggsPerMonth();
+        System.out.println(hen3.getDescription());
     }
 
-    static class HenFactory {
+    static class HenFactory implements Country {
 
         static Hen getHen(String country) {
             Hen hen = null;
             //напишите тут ваш код
+
+            if (country.equals(Country.BELARUS)) {
+                hen = new BelarusianHen();
+            } else if (country.equals(Country.MOLDOVA)) {
+                hen = new MoldovanHen();
+            } else if (country.equals(Country.UKRAINE)) {
+                hen = new UkrainianHen();
+            } else if (country.equals(Country.RUSSIA)) {
+                hen = new RussianHen();
+            }
             return hen;
         }
-    }*/
-
+    }
 
 }
