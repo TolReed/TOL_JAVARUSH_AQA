6ab7c4cb35f69eef76b0f3b612a9c1871a9b89f8
TolReed <tolreed@gmail.com>
2020-01-10 17:05:19 +0200

    14.19 - 10 exceptions

--- 2.JavaCore/src/com/javarush/task/task14/task1419/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1419/Solution.java
@@ -1,5 +1,9 @@
 package com.javarush.task.task14.task1419;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +22,7 @@ public class Solution {
         }
     }
 
-    private static void initExceptions() {   //the first exception
+    private static void initExceptions() throws TypeNotPresentException {   //the first exception
         try {
             float i = 1 / 0;
 
@@ -27,6 +31,65 @@ public class Solution {
         }
 
         //напишите тут ваш код
+        try {
+            throw new IOException();
+        } catch (IOException e) {
+            exceptions.add(e);
+        }
+
+        try {
+            FileInputStream fis = new FileInputStream("");
+            BufferedReader bis = new BufferedReader(new InputStreamReader(fis));
+            fis.close();
+        } catch (Exception e) {
+            exceptions.add(e);
+        }
+
+        try {
+            Integer a = Integer.parseInt("строка");
+        } catch (Exception e) {
+            exceptions.add(e);
+        }
+
+        try {
+            throw new RuntimeException();
+        } catch (Exception e) {
+            exceptions.add(e);
+        }
+
+        try {
+            throw new UnsupportedOperationException ();
+        } catch (UnsupportedOperationException e) {
+            exceptions.add(e);
+        }
+
+        try {
+            Object o[] = new String[3];
+            o[0] = new Integer(0);
+        } catch (Exception e) {
+            exceptions.add(e);
+        }
+
+        try {
+            Object o = null;
+            o.equals("tratata");
+        } catch (Exception e) {
+            exceptions.add(e);
+        }
+
+        try {
+            throw new IllegalArgumentException();
+        } catch (IllegalArgumentException e) {
+            exceptions.add(e);
+        }
+
+        try {
+            throw new ArrayIndexOutOfBoundsException ();
+        } catch (ArrayIndexOutOfBoundsException  e) {
+            exceptions.add(e);
+        }
+
+
 
     }
 }
