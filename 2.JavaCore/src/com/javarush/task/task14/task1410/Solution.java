5683e29344764ed35bfe41aa1062267a0132355f
TolReed <tolreed@gmail.com>
2020-01-10 15:10:19 +0200

    14.10 - abstract class Drink, Wine, BubblyWine - separate files create

--- 2.JavaCore/src/com/javarush/task/task14/task1410/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/Solution.java
@@ -4,7 +4,7 @@ package com.javarush.task.task14.task1410;
 Дегустация вин
 */
 
-public class Solution {/*
+public class Solution {
     public static void main(String[] args) {
         getDeliciousDrink().taste();
         System.out.println(getWine().getHolidayName());
@@ -13,14 +13,17 @@ public class Solution {/*
     }
 
     public static Drink getDeliciousDrink() {
+        return new Wine();
 
     }
 
     public static Wine getWine() {
+        return new Wine();
 
     }
 
     public static Wine getBubblyWine() {
+        return new BubblyWine();
 
-    }*/
+    }
 }
