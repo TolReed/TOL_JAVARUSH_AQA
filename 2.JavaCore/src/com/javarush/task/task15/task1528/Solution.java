d18a6f26bf47829746ab6d6060794286fb170a39
TolReed <tolreed@gmail.com>
2020-01-10 17:36:06 +0200

    15.28 - wow - Hrivna - public Object

--- 2.JavaCore/src/com/javarush/task/task15/task1528/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1528/Solution.java
@@ -19,28 +19,15 @@ public class Solution {
 
     //add your code below - добавь код ниже
     public static class Hrivna extends Money {
-        private Object amount = 123d;
+        private double amount = 123d;
 
         public Hrivna getMoney() {
             return this;
         }
 
         @Override
-        public Object getAmount() {
+        public Object getAmount() { //change
             return amount;
         }
     }
 }
-
-
-/*
-Comments:
-        2. Метод позволяет вернуть объект целиком.
-        1. На объекте что возвращает метод из пункта 2 вы можете
-        вызвать метод getAmount() , который в свою очередь позволит вернуть значение amount.
-        3. Вы можете изменить на любой тип (Integer, Double, String..), задача будет работать, хотя как поведет себя валидатор не известно) .
-        Благодаря возвращаемому параметру Object, в абстрактном классе, вы можете переопределить
-        метод и указать любой тип возвращаемого значения, так как все классы наследуются от Object.
-
-        ! В обоих методах getAmount замените double на Object.
-*/
