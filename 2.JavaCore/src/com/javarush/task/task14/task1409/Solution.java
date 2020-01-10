4ebe2b71dee22f3c6aa3f656b036334ece06bdc8
TolReed <tolreed@gmail.com>
2020-01-10 14:59:00 +0200

    14.09 - Classes WaterBridge и SuspensionBridge implement interface Bridge.

--- 2.JavaCore/src/com/javarush/task/task14/task1409/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1409/Solution.java
@@ -4,12 +4,17 @@ package com.javarush.task.task14.task1409;
 Мосты
 */
 
-public class Solution {/*
+public class Solution {
     public static void main(String[] args) {
         println(new WaterBridge());
         println(new SuspensionBridge());
-    }*/
+    }
 
     //add println method here
+    public static void println(Bridge bridge) {
+        System.out.println(bridge.getCarsCount());
+
+    }
+
 }
 
