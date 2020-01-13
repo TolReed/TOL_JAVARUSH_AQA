1acf4944d4e3cd28f523f284ada5b8c8849b54db
TolReed <tolreed@gmail.com>
2020-01-13 16:33:54 +0200

    14.14 - MovieFactory - wow

--- 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -4,6 +4,9 @@ package com.javarush.task.task14.task1414;
 MovieFactory
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
         //ввести с консоли несколько ключей (строк), пункт 7
@@ -13,6 +16,37 @@ public class Solution {
 8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
 8.2 вывести на экран movie.getClass().getSimpleName()
         */
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String key = null;
+        Movie movie = null;
+
+        while (true) {
+
+            key = reader.readLine();
+
+            if ("cartoon".equals(key)) {
+                movie = MovieFactory.getMovie(key);
+                System.out.println(movie.getClass().getSimpleName());
+            }
+
+            else if ("soapOpera".equals(key)) {
+                movie = MovieFactory.getMovie(key);
+                System.out.println(movie.getClass().getSimpleName());
+            }
+
+            else if ("thriller".equals(key)) {
+                movie = MovieFactory.getMovie(key);
+                System.out.println(movie.getClass().getSimpleName());
+            }
+
+            else {
+                movie = MovieFactory.getMovie(key);
+                break;
+            }
+
+
+        }
 
     }
 
@@ -27,6 +61,13 @@ public class Solution {
             }
 
             //напишите тут ваш код, пункты 5,6
+            else if ("cartoon".equals(key)) {
+                movie = new Cartoon();
+            }
+
+            else if ("thriller".equals(key)) {
+                movie = new Thriller();
+            }
 
             return movie;
         }
@@ -39,4 +80,9 @@ public class Solution {
     }
 
     //Напишите тут ваши классы, пункт 3
+    static class Cartoon extends Movie {
+    }
+
+    static class Thriller extends Movie {
+    }
 }
