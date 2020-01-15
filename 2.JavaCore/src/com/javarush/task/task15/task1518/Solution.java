63025261df2a4f3dc8cbd9eb32b91a8074a47f5f
TolReed <tolreed@gmail.com>
2020-01-15 17:02:27 +0200

    15.18 - static

--- 2.JavaCore/src/com/javarush/task/task15/task1518/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1518/Solution.java
@@ -4,10 +4,30 @@ package com.javarush.task.task15.task1518;
 Статики и котики
 */
 
-public class Solution {/*
-    public static Cat cat;
+public class Solution {
+    public static Cat cat; //В статическом блоке создай объект типа Cat и присвой его переменной cat (не забудь инициализировать поле name).
+
 
     public static void main(String[] args) {
 
-    }*/
+    }
+
+    public static class Cat {
+        public String name;
+    }
+
+    static {
+        cat = new Cat();
+        cat.name = "Tol";
+        System.out.println(cat.name); //В статическом блоке выведи имя созданного кота на экран.
+
+    }
+
+
 }
+
+/*
+Cat cat; // создание переменной.
+cat = new Cat(); // инициализация переменной.
+Cat cat = new Cat(); // создание и инициализация сразу.
+*/
