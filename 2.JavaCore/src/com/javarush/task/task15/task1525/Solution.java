e9196d3e2f97a631a93986098e610a7cf18098b1
TolReed <tolreed@gmail.com>
2020-01-17 15:45:15 +0200

    15.25 - wow - read from FILE_NAME

--- 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
@@ -1,11 +1,11 @@
 package com.javarush.task.task15.task1525;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /* 
 Файл в статическом блоке
@@ -13,6 +13,20 @@ import java.util.List;
 
 public class Solution {
     public static List<String> lines = new ArrayList<String>();
+
+    static {
+
+        try {
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(Statics.FILE_NAME)));
+            String str;
+            while ((str = reader.readLine()) != null) {
+                lines.add(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(lines);
--- 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -1,5 +1,39 @@
 package com.javarush.task.task15.task1525;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Statics {
-    public static String FILE_NAME = "";/* add the path to your source file here */;
+    public static String FILE_NAME = "/Users/anatolii.dudochkin/Desktop/Tol/tol.txt";/* add the path to your source file here */
+
+
+
+    ;
 }
+/* Тоже интересные варианты:
+
+1. Files.lines(Paths.get(Statics.FILE_NAME)).forEach(line -> lines.add(line));
+2. lines = Files.readAllLines(Paths.get(Statics.FILE_NAME)));
+Comment: В первом с каждой строкой отдельно работает, а во втором варианте сразу отдаёт в виде List<String>
+3. вариации на тему collect
+lines = new BufferedReader(new FileReader(Statics.FILE_NAME)).lines().collect(Collectors.toList());
+
+> Полина22 уровень
+    я до сих пор не понимаю, для чего нужен статический блок. Объясните кто-нибудь своими словами, пожалуйста
+
+> Иван Ганжа20 уровень, Москва
+    Нет проблем проинициализировать статическую переменную при написания класса
+    public static counter = 0;
+    Но если надо проинициализировать статическую переменную "на лету", то есть мы при написании класса не знаем чему переменная будет равна.
+    Синтетический пример: Тот же counter только начинатся он должен не с 0, а с числа которое рассчитывается исходя из температуры за окном на момент загрузки класса.
+    Вот тогда и нужен блок статик, что бы загрузиться температуру из интернета и проинициализировать каунтер.
+
+> Denis Sabinin26 уровень, Москва
+    Для удобства, простоты, гибкости, вариативности и скорости написания кода
+    по- сути это неименованный static private void метод без параметров, который тут-же описан и тут-же вызывается.
+*/
