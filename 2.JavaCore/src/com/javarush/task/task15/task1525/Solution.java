e42136d790fc220998dbe671b29a5a370c2c2793
TolReed <tolreed@gmail.com>
2020-01-17 16:06:38 +0200

    15.25 - wow Statics.FILE_NAME все строки и добавь их по отдельности в List lines и добавь их по отдельности в List lines > cho-cho

--- 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
@@ -14,14 +14,14 @@ import java.util.stream.Collectors;
 public class Solution {
     public static List<String> lines = new ArrayList<String>();
 
-    static { // В статическом блоке считай из файла с именем Statics.FILE_NAME все строки и добавь их по отдельности в List lines.
+    static { // В статическом блоке считай из файла с именем Statics.FILE_NAME все строки и добавь их по отдельности в List lines
 
         try {
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(Statics.FILE_NAME)));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(Statics.FILE_NAME))); // < считай из файла с именем Statics.FILE_NAME
             String str;
 
-            while ((str = reader.readLine()) != null) {
+            while ((str = reader.readLine()) != null) { // < считай из файла с именем Statics.FILE_NAME все строки и добавь их по отдельности в List lines и добавь их по отдельности в List lines
                 lines.add(str);
             }
         } catch (IOException e) {
