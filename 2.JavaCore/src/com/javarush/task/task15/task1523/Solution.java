f9f7dc0eb6c584aaea5672cf225bec75a1390db2
TolReed <tolreed@gmail.com>
2020-01-17 14:50:43 +0200

    15.23 - SubSolution - Alt+Insert -> Constructors

--- 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -5,8 +5,26 @@ package com.javarush.task.task15.task1523;
 */
 
 public class Solution {
+
+
+    public  Solution() {
+
+    }
+
+    protected Solution (int a ) {
+    }
+
+    private Solution (int a, int b) {
+    }
+
+    Solution (int a, int b, int c) {
+
+    }
+
     public static void main(String[] args) {
 
     }
-}
 
+
+
+}
