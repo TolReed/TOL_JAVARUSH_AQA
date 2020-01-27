6671513c181073ed25e626dd2715cc5a4f5c3f06
TolReed <tolreed@gmail.com>
2020-01-27 17:21:50 +0200

    16.19 - А без interrupt слабо?

--- 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
@@ -4,7 +4,10 @@ package com.javarush.task.task16.task1619;
 А без interrupt слабо?
 */
 
-public class Solution {
+public class Solution { // https://javarush.ru/help/20341
+
+
+    public static boolean isTestRun = true;
     public static void main(String[] args) throws InterruptedException {
         Thread t = new Thread(new TestThread());
         t.start();
@@ -13,12 +16,13 @@ public class Solution {
     }
 
     public static void ourInterruptMethod() {
+        isTestRun = false;
 
     }
 
     public static class TestThread implements Runnable {
         public void run() {
-            while (true) {
+            while (isTestRun) {
                 try {
                     System.out.println("he-he");
                     Thread.sleep(500);
