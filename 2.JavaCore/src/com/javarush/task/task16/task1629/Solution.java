82e88741a349615893a36b61a0f0183e8b1a0ef2
TolReed <tolreed@gmail.com>
2020-01-29 16:02:21 +0200

    16.29 - wow - Read3Strings

--- 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
@@ -3,8 +3,9 @@ package com.javarush.task.task16.task1629;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
-public class Solution {/*
+public class Solution {
     public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws InterruptedException {
@@ -12,10 +13,37 @@ public class Solution {/*
         Read3Strings t2 = new Read3Strings();
 
         //add your code here - добавьте код тут
+        t1.start();
+        t1.join();
+
+        t2.start();
+        t2.join();
 
         t1.printResult();
         t2.printResult();
-    }*/
+    }
 
     //add your code here - добавьте код тут
+    public static class Read3Strings extends Thread {
+        ArrayList<String> stringResult = new ArrayList<>();
+
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < 3; i++) {
+                    stringResult.add(reader.readLine());
+                }
+            } catch (Exception e) {
+
+            }
+        }
+
+        public void printResult() {
+            for (String lol: stringResult) {
+                System.out.print(lol + " ");
+            }
+            System.out.println();
+
+        }
+    }
 }
