4e5e9db08fa4464ccd34133c68b3b7c7394ab6c9
TolReed <tolreed@gmail.com>
2020-01-30 13:22:46 +0200

    17.11 - o_0

--- 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -1,14 +1,17 @@
 package com.javarush.task.task17.task1710;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 // https://javarush.ru/help/34279
 /* 
 CRUD
 */
 
-public class Solution {
+public class Solution { //https://javarush.ru/help/29258 & https://javarush.ru/help/33027
     public static List<Person> allPeople = new ArrayList<Person>();
 
     static {
@@ -16,7 +19,42 @@ public class Solution {
         allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         //start here - начни тут
+        SimpleDateFormat oldFormat = new SimpleDateFormat("dd/mm/yyyy");
+        SimpleDateFormat newFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+
+        System.out.println(args[0] + " " + args[1] + " " + args[2] + " " + args[3]);
+
+        if(args[0].equals("-c")) {
+            Person person;
+            System.out.println(args[3]);
+            if(args[2].equals("м")) {
+                person = Person.createMale(args[1],oldFormat.parse(args[3]));
+            }else {
+                person = Person.createFemale(args[1],oldFormat.parse(args[3]));
+            }
+            allPeople.add(person);
+            System.out.println(allPeople.indexOf(person));
+        }else if(args[0].equals("-u")) {
+            Person person;
+            if(args[3].equals("м")) {
+                person = Person.createMale(args[2],oldFormat.parse(args[4]));
+            }else {
+                person = Person.createFemale(args[2],oldFormat.parse(args[4]));
+            }
+            allPeople.set(Integer.parseInt(args[1]),person);
+        }else if(args[0].equals("-d")) {
+            int index = Integer.parseInt(args[1]);
+            Person person = allPeople.get(index);
+            person.setName(null);
+            person.setSex(null);
+            person.setBirthDate(null);
+        }else if(args[0].equals("-i")) {
+            int index = Integer.parseInt(args[1]);
+            Person person = allPeople.get(index);
+            String newDate = newFormat.format(person.getBirthDate());
+            System.out.println(person.getName() + " " + person.getSex() + " " + newDate);
+        }
     }
 }
--- 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
@@ -1,9 +1,13 @@
 package com.javarush.task.task17.task1711;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+import java.util.Locale;
+//https://javarush.ru/help/32988
 /* 
 CRUD 2
 */
@@ -16,7 +20,60 @@ public class Solution {
         allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
     }
 
-    public static void main(String[] args) {
-        //start here - начни тут
+
+    public static void main(String[] args) throws IOException, ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        SimpleDateFormat dateOut = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+
+        switch (args[0]) {
+
+            case "-c":
+                synchronized (allPeople) {
+                    for (int i = 1, j = 0; j < (args.length - 1) / 3; i += 3, j++) {
+                            if (args[i+1].equals("м")) {
+                            allPeople.add(Person.createMale(args[i], formatter.parse(args[i + 2])));
+                            System.out.println(allPeople.size() - 1);
+                        } else {
+                            allPeople.add(Person.createFemale(args[i], formatter.parse(args[i + 2])));
+                            System.out.println(allPeople.size() - 1);
+                        }
+                    }
+                    break;
+                }
+            case "-u":
+                synchronized (allPeople) {
+                    for (int i = 1, j = 0; j < (args.length - 1) / 4; i += 4, j++) {
+                        allPeople.get(Integer.parseInt(args[i])).setName(args[i + 1]);
+                        if (args[i+2].equals("м")) {
+                            allPeople.get(Integer.parseInt(args[i])).setSex(Sex.MALE);
+                        } else {
+                            allPeople.get(Integer.parseInt(args[i])).setSex(Sex.FEMALE);
+                        }
+                        allPeople.get(Integer.parseInt(args[i])).setBirthDate(formatter.parse(args[i + 3]));
+                    }
+                    break;
+                }
+            case "-d":
+                synchronized (allPeople) {
+                    for (int i = 1; i < args.length; i++) {
+                        allPeople.get(Integer.parseInt(args[i])).setName(null);
+                        allPeople.get(Integer.parseInt(args[i])).setBirthDate(null);
+                        allPeople.get(Integer.parseInt(args[i])).setSex(null);
+                    }
+                    break;
+                }
+            case "-i":
+                synchronized (allPeople) {
+                    for (int i = 1; i < args.length; i++) {
+                        if (allPeople.get(Integer.parseInt(args[i])).getSex().equals(Sex.MALE)) {
+                            System.out.println(allPeople.get(Integer.parseInt(args[i])).getName() + " м " + dateOut.format(allPeople.get(Integer.parseInt(args[i])).getBirthDate()));
+                        } else {
+                            System.out.println(allPeople.get(Integer.parseInt(args[i])).getName() + " ж " + dateOut.format(allPeople.get(Integer.parseInt(args[i])).getBirthDate()));
+                        }
+                    }
+                    break;
+                }
+        }
     }
 }
+
