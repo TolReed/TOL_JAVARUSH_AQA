648a3c1f418f9556f363da137a971a1dea94b218
TolReed <tolreed@gmail.com>
2020-01-30 18:14:02 +0200

    17.21 - wow

--- 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -1,5 +1,9 @@
 package com.javarush.task.task17.task1721;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,9 +16,33 @@ public class Solution {
     public static List<String> forRemoveLines = new ArrayList<String>();
 
     public static void main(String[] args) {
+        try ( BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+              BufferedReader fileR1 = new BufferedReader(new FileReader(reader.readLine()));
+              BufferedReader fileR2 = new BufferedReader(new FileReader(reader.readLine())) ) {
+            reader.close();
+            while (fileR1.ready()) {
+                allLines.add(fileR1.readLine());
+            }
+            fileR1.close();
+            while (fileR2.ready()) {
+                forRemoveLines.add(fileR2.readLine());
+            }
+            fileR2.close();
+            new Solution().joinData();
+
+        } catch (IOException exc) {
+
+        }
     }
 
     public void joinData() throws CorruptedDataException {
-
+        if (allLines != null && forRemoveLines != null) {
+            if (!allLines.containsAll(forRemoveLines)) {
+                allLines.clear();
+                throw new CorruptedDataException();
+            } else {
+                allLines.removeAll(forRemoveLines);
+            }
+        }
     }
 }
