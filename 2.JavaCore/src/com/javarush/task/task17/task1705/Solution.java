e0cc8d4694a265a74be7ea4772cf2daaade59476
TolReed <tolreed@gmail.com>
2020-01-30 12:38:48 +0200

    17.05 - fruits

--- 2.JavaCore/src/com/javarush/task/task17/task1705/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1705/Solution.java
@@ -19,5 +19,38 @@ public class Solution {
         public final List<String> fruits = new ArrayList<String>();
         public final List<String> vegetables = new ArrayList<String>();
 
+
+        public synchronized void addFruit(int index, String fruit) {
+            synchronized (this) {
+                fruits.add(index, fruit);
+            }
+        }
+
+        public synchronized void removeFruit(int index) {
+            synchronized (this) {
+                fruits.remove(index);
+            }
+        }
+
+
+
+        public synchronized void addVegetable(int index, String vegetable) {
+            synchronized (this) {
+                vegetables.add(index, vegetable);
+            }
+        }
+
+        public synchronized void removeVegetable(int index) {
+            synchronized (this) {
+                vegetables.remove(index);
+            }
+        }
+
+
+
+
+
     }
+
+
 }
