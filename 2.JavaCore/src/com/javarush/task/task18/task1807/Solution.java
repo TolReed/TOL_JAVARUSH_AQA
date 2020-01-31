532f07e4b2f77e7aae89077c7bd22fff37038981
TolReed <tolreed@gmail.com>
2020-01-31 16:20:54 +0200

    18.07 - comma count

--- 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
@@ -4,7 +4,27 @@ package com.javarush.task.task18.task1807;
 Подсчет запятых
 */
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream inputStream = new FileInputStream(reader.readLine());
+
+        int zapitie = 0;
+        while (inputStream.available() > 0) {
+            if (inputStream.read() != (',')) {
+                continue;
+            } else {
+                zapitie++;
+            }
+
+        }
+        reader.close();
+        inputStream.close();
+        System.out.println(zapitie);
     }
 }
