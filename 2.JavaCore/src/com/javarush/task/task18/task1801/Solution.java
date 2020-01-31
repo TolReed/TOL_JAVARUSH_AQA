770029a7e2c88d5b76cc620860a6c5ccee985489
TolReed <tolreed@gmail.com>
2020-01-31 15:42:39 +0200

    18.01 - FileInputStream MAX

--- 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task18.task1801;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /* 
 Максимальный байт
@@ -8,5 +13,21 @@ import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        FileInputStream fileInputStream = new FileInputStream(name);
+
+        ArrayList<Integer> bytes = new ArrayList<>();
+
+        while (fileInputStream.available() > 0) {
+            bytes.add(fileInputStream.read());
+        }
+
+        fileInputStream.close();
+
+        System.out.println(Collections.max(bytes));
+
     }
 }
