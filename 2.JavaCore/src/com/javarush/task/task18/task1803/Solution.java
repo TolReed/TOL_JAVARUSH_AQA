19168db3474ee7a4137b8a873237909046205575
TolReed <tolreed@gmail.com>
2020-01-31 15:51:46 +0200

    18.03 - console read file name > search for max repeat

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task18.task1803;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /* 
 Самые частые байты
@@ -8,5 +11,32 @@ import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+
+        int[] bytes = new int[255];
+        BufferedReader filename = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream is = new FileInputStream(filename.readLine());
+        byte b;
+        while (is.available() > 0) {
+            b = (byte)is.read();
+            bytes[b]++;
+        }
+        ArrayList<Integer> maxBytes = new ArrayList<>();
+        int max = 1;
+        for (int i = 0; i < bytes.length; i++){
+            if (bytes[i] > max){
+                max = bytes[i];
+            }
+        }
+        for (int i = 0; i < bytes.length; i++){
+            if (bytes[i] == max){
+                maxBytes.add(i);
+            }
+        }
+        for (Integer maxByte : maxBytes) {
+            System.out.print(maxByte + " ");
+        }
+        filename.close();
+        is.close();
     }
 }
