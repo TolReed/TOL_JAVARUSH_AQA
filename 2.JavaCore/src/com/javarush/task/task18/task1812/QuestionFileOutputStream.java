8de4ad46cdf388321641529bd277af87691aea0e
TolReed <tolreed@gmail.com>
2020-01-31 17:11:47 +0200

    18.12 - QuestionFileOutputStream - wow

--- 2.JavaCore/src/com/javarush/task/task18/task1812/QuestionFileOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1812/QuestionFileOutputStream.java
@@ -6,7 +6,41 @@ import java.io.*;
 Расширяем AmigoOutputStream
 */
 
-/*public class QuestionFileOutputStream implements AmigoOutputStream {
+public class QuestionFileOutputStream implements AmigoOutputStream {
+    AmigoOutputStream name;
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-}*/
+    public QuestionFileOutputStream(AmigoOutputStream name) {
+        this.name = name;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        name.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        name.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        name.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        name.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Вы действительно хотите закрыть поток? Д/Н");
+        if (reader.readLine().equals("Д")) {
+            name.close();
+        }
+
+    }
+}
 
