f26d3a468e4e95e81ab6bdbde6ba84f79f8d6d2b
TolReed <tolreed@gmail.com>
2020-01-31 16:01:46 +0200

    18.04 - Самые редкие байты

--- 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -1,6 +1,12 @@
 package com.javarush.task.task18.task1804;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /* 
 Самые редкие байты
@@ -8,5 +14,43 @@ import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fReader = reader.readLine();
+
+        FileInputStream fileInputStream = new FileInputStream(fReader);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (fileInputStream.available() > 0) {
+            list.add(fileInputStream.read());
+        }
+        fileInputStream.close();
+
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.print(list.get(i) + " ");
+//        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            map.put(list.get(i), Collections.frequency(list, list.get(i)));
+        }
+
+        int min = map.size();
+        //System.out.println(min);
+        for (Integer value : map.values()) {
+            if (min > value) {
+                min = value;
+            }
+        }
+
+        //System.out.println(min);
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == min) {
+                System.out.print(entry.getKey() + " ");
+            }
+        }
     }
 }
