405191349926ffef772529d0318db798c834424f
TolReed <tolreed@gmail.com>
2020-01-31 15:45:08 +0200

    18.02 - FileInputStream MIN

--- 2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task18.task1802;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /* 
 Минимальный байт
@@ -8,5 +12,18 @@ import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        FileInputStream fileInputStream = new FileInputStream(name);
+
+        ArrayList<Integer> bytes = new ArrayList<>();
+
+        while (fileInputStream.available() > 0) {
+            bytes.add(fileInputStream.read());
+        }
+
+        fileInputStream.close();
+        System.out.println(Collections.min(bytes));
     }
 }
