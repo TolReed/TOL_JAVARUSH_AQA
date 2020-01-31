2295e2779bfc3e2d751cd9f96ab187608e48cda0
TolReed <tolreed@gmail.com>
2020-01-31 16:34:31 +0200

    18.09 - Реверс файла

--- 2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
@@ -1,11 +1,51 @@
 package com.javarush.task.task18.task1809;
 
 /* 
-Реверс файла
+Реверс файла - https://javarush.ru/help/33324
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String firstFileName = reader.readLine();
+        String secondFileName = reader.readLine();
+
+        FileInputStream inputStream = new FileInputStream(firstFileName);
+        FileOutputStream outputStream1 = new FileOutputStream(secondFileName);
+
+        if(inputStream.available() > 0){
+            byte[] buff = new byte[inputStream.available()];
+            int count = inputStream.read(buff);
+            for(int i = buff.length - 1; i >= 0; i--){
+                outputStream1.write(buff[i]);
+            }
+        }
+
+        reader.close();
+        inputStream.close();
+        outputStream1.close();
 
     }
+
+
 }
+
+/* dec sort
+static void sort(byte[] buff){
+        byte tmp = 0;
+        for(int i = buff.length-1; i > 0; i--){
+            for(int j = 0; j < i; j++){
+                if(buff[j] < buff[j + 1]) {
+                    tmp = buff[j];
+                    buff[j] = buff[j + 1];
+                    buff[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
+
+*/
