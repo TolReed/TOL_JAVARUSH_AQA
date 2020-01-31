9d4f49b6ff38efc0b21fa92239e578b5f5a97789
TolReed <tolreed@gmail.com>
2020-01-31 18:01:23 +0200

    18.16 - English
    Английские буквы
    В метод main первым параметром приходит имя файла.
    Посчитать количество букв английского алфавита, которое есть в этом файле.
    Вывести на экран число (количество букв).
    Закрыть потоки.

--- 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
@@ -3,8 +3,25 @@ package com.javarush.task.task18.task1816;
 /* 
 Английские буквы
 */
+// https://javarush.ru/help/33074
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int simbol = 0;
+        FileInputStream inFile = new FileInputStream(args[0]);
+        int i;
+        String stw;
+        StringBuilder builder = new StringBuilder();
+        while ((i = inFile.read()) != -1) {
+            builder.append((char) i);
+            stw = builder.toString();
+            stw = stw.replaceAll("\\p{Cntrl}", " ");
+            if (stw.matches(".*[a-zA-Z]")) simbol++;
+        }
+        //System.out.print(stw);
+        inFile.close();
+        System.out.print(simbol);
     }
 }
