47350fa78fdf4544035224cf0f2ce3669a4b7871
TolReed <tolreed@gmail.com>
2020-02-03 15:59:58 +0200

    18.23 - wow

--- 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
@@ -1,7 +1,9 @@
 package com.javarush.task.task18.task1823;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
+
+import static com.javarush.task.task18.task1823.Solution.resultMap;
 
 /* 
 Нити и байты
@@ -10,14 +12,72 @@ import java.util.Map;
 public class Solution {
     public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file;
+
+        while (!(file = reader.readLine()).equals("exit")) {
+            ReadThread thread = new ReadThread(file);
+            thread.start();
+        }
+        //for(Map.Entry m : resultMap.entrySet()) System.out.println(m.getKey() + ": " + m.getValue());
     }
 
     public static class ReadThread extends Thread {
+
+        String fileName;
+
         public ReadThread(String fileName) {
             //implement constructor body
+            this.fileName = fileName;
         }
+
         // implement file reading here - реализуйте чтение из файла тут
+        public void run() {
+            try {
+                BufferedReader filereader = new BufferedReader(new FileReader(fileName));
+                int b;
+                List<Integer> integers = new ArrayList<>();
+                TreeMap<Integer, Integer> map = new TreeMap<>();
+
+
+                while ((b = filereader.read()) != -1) {
+                    integers.add(b);
+                }
+
+                int count;
+
+                for (Integer integer : integers) {
+                    if (map.containsKey(integer)) {
+                        count = map.get(integer);
+                        map.put(integer, count + 1);
+                    } else {
+                        map.put(integer, 1);
+                    }
+                }
+
+                int max = 0;
+                int by = 0;
+
+                for (Map.Entry<Integer, Integer> item : map.entrySet()) {
+                    if (item.getValue() > max) {
+                        by = item.getKey();
+                        max = item.getValue();
+                    }
+
+                }
+
+                resultMap.put(fileName, by);
+
+                filereader.close();
+            } catch (IOException ioex) {
+                ioex.getStackTrace();
+            }
+
+        }
     }
+
+
 }
