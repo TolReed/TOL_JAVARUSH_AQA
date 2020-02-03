6854fbce2232b491e97b430fc0f22c4b6377888d
TolReed <tolreed@gmail.com>
2020-02-03 16:17:42 +0200

    18.25 - wow

--- 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
@@ -5,15 +5,37 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /* 
 Собираем файл
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = "";
+        List<String> list = new ArrayList<>();
+        do {
+            if ((s = br.readLine()).equals("end")) {
+                break;
+            }
+            list.add(s);
+        } while (true);
+        br.close();
+
+        Collections.sort(list);
+        String path = list.get(0).replaceAll("\\.part\\d+$", "");
+        FileOutputStream bw = new FileOutputStream(path, true);
+        for (String s1 : list) {
+            FileInputStream fis = new FileInputStream(s1);
+            byte[] tmp = new byte[fis.available()];
+            fis.read(tmp);
+            bw.write(tmp);
+            bw.flush();
+            fis.close();
+        }
+        bw.close();
     }
 }
