657834930c8cffc665b92818130590ccf418aa17
TolReed <tolreed@gmail.com>
2020-02-03 15:52:21 +0200

    18.22 - wow

--- 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -4,7 +4,23 @@ package com.javarush.task.task18.task1822;
 Поиск данных внутри файла
 */
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader file = new BufferedReader(new FileReader(reader.readLine()));
+
+        while (file.ready()){
+            String s = file.readLine();
+            String[] str  = s.split(" ");
+            if (str[0].equals(args[0])){
+                System.out.println(s);
+            }
+        }
+        file.close();
+        reader.close();
     }
 }
