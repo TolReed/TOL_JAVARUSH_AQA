f7c0490400d025546119d40ae3e18586bda4a00c
TolReed <tolreed@gmail.com>
2020-02-05 17:26:25 +0200

    19.04 - PersonScannerAdapter - regexp

--- 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task19.task1904;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 /* 
@@ -9,11 +12,47 @@ import java.util.Scanner;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
     }
 
-    public static class PersonScannerAdapter {
+    public static class PersonScannerAdapter implements PersonScanner {
+
+        private static final String PERSON_REGEXP  = "(.+) (.+) (.+) (\\d+) (\\d+) (\\d+)";
+        private static final String DATE_FORMATTER = "dd MM yyyy";
+        private Scanner fileScanner;
+
+        public PersonScannerAdapter(Scanner fileScanner) {
+            this.fileScanner = fileScanner;
+        }
+
+        @Override
+        public Person read() throws IOException {
+
+            if (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String firstName  = line.replaceFirst(PERSON_REGEXP, "$2");
+                String middleName = line.replaceFirst(PERSON_REGEXP, "$3");
+                String lastName   = line.replaceFirst(PERSON_REGEXP, "$1");
+                SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMATTER);
+                Date birthDate = null;
+                try {
+                    birthDate = sdf.parse(line.replaceFirst(PERSON_REGEXP, "$4 $5 $6"));
+                }
+                catch (ParseException e) {e.printStackTrace();}
+
+                return new Person(firstName, middleName, lastName, birthDate);
+            }
+
+            return null;
+
+        }
+
+        @Override
+        public void close() throws IOException {
+            fileScanner.close();
+        }
+
 
     }
 }
