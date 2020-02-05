c473dbca3323b9b522683066bccb42a4af88c694
TolReed <tolreed@gmail.com>
2020-02-05 17:53:41 +0200

    19.05 - "callto://+"+contact.getPhoneNumber().replaceAll("\\D","");

--- 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
@@ -1,21 +1,65 @@
 package com.javarush.task.task19.task1905;
 
+import com.sun.org.apache.bcel.internal.classfile.Constant;
+
 import java.util.HashMap;
 import java.util.Map;
 
-/* 
+/*
 Закрепляем адаптер
 */
 
 public class Solution {
     public static Map<String,String> countries = new HashMap<String,String>();
 
-    public static void main(String[] args) {
-
+    static {
+        countries.put("UA","Ukraine");
+        countries.put("RU","Russia");
+        countries.put("CA","Canada");
     }
 
-    public static class DataAdapter {
+    public static void main(String[] args) {
+    }
+
+    public static class DataAdapter implements RowItem{
+        private Customer customer;
+        private Contact contact;
+
         public DataAdapter(Customer customer, Contact contact) {
+            this.customer=customer;
+            this.contact=contact;
+        }
+
+        @Override
+        public String getCountryCode() {
+            String s=null;
+            for (Map.Entry<String ,String > get:countries.entrySet()){
+                if (get.getValue().equals(customer.getCountryName())){
+                    s=get.getKey();
+                    break;
+                }
+            }
+            return s;
+        }
+
+        @Override
+        public String getCompany() {
+            return customer.getCompanyName();
+        }
+
+        @Override
+        public String getContactFirstName() {
+            return contact.getName().split(", ")[1];
+        }
+
+        @Override
+        public String getContactLastName() {
+            return contact.getName().split(", ")[0];
+        }
+
+        @Override
+        public String getDialString() {
+            return "callto://+"+contact.getPhoneNumber().replaceAll("\\D","");
         }
     }
 
