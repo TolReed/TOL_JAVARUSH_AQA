fcb3d65c5489c009d052ceff2ad0199adf2d591e
TolReed <tolreed@gmail.com>
2020-02-06 16:12:25 +0200

    34.03 - recurse
    Разложение на множители с помощью рекурсии
    Разложить целое число n > 1 на простые множители.
    Вывести в консоль через пробел все множители в порядке возрастания

--- 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
@@ -4,16 +4,18 @@ package com.javarush.task.task34.task3404;
 Рекурсия для мат. выражения
 */
 public class Solution {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.recurse("sin(2*(-5+1.5*4)+28)", 0); //expected output 0.5 6
-    }
+    public void recursion(int n) {
+        int m = 2;
+        while (m <= n) {
+            if (n % m == 0) {
+                System.out.print(m + " ");
+                if (m == n)
+                    return;
+                recursion(n/m);
+                break;
+            }
+            m++;
+        }
 
-    public void recurse(final String expression, int countOperation) {
-        //implement
-    }
-
-    public Solution() {
-        //don't delete
     }
 }
