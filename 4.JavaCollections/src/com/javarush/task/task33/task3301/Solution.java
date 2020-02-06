d3bfd495ff75153339fb2af8ed643b93edf0dae9
TolReed <tolreed@gmail.com>
2020-02-06 17:03:58 +0200

    33.01 - o_0

--- 4.JavaCollections/src/com/javarush/task/task33/task3301/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3301/Solution.java
@@ -9,6 +9,16 @@ import java.util.ArrayList;
 
 /*
 Первая сериализация в JSON
+НЕОБХОДИМО: подключенные библиотеки Jackson Core, Bind и Annotation версии 2.6.1
+1) В программе не выполнено основное требование к сериализации в JSON.
+Найди ошибку и исправь.
+2) Расставь правильно JSON аннотации у классов.
+Все данные должны сериализоваться.
+Требования:
+1. Класс Pet должен быть помечен как готовый к сериализации в JSON.
+2. Класс Cat должен быть помечен как готовый к сериализации в JSON.
+3. Класс Dog должен быть помечен как готовый к сериализации в JSON.
+4. Все данные у классов Pet, Cat, Dog должны сериализоваться.
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
@@ -37,13 +47,20 @@ public class Solution {
         mapper.writeValue(writer, object);
     }
 
+    /*@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property="type")
+    @JsonSubTypes({
+            @JsonSubTypes.Type(value=Cat.class, name="cat"),
+            @JsonSubTypes.Type(value=Dog.class, name="dog")
+    })*/
     @JsonAutoDetect
     public static class Pet {
         public String name;
+        Pet () {}
     }
 
     @JsonAutoDetect
     public static class Cat extends Pet {
+        Cat (){}
         public int age;
         public int weight;
     }
@@ -52,5 +69,6 @@ public class Solution {
     public static class Dog extends Pet {
         public int age;
         public String owner;
+        public Dog () {}
     }
 }
