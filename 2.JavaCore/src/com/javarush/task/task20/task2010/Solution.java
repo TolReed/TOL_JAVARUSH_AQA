7cc6e0bf1de6f40572aa3dd97aa7ce0844d5fad0
TolReed <tolreed@gmail.com>
2020-02-07 18:17:46 +0200

    20.10 - Как сериализовать что-то свое?

--- 2.JavaCore/src/com/javarush/task/task20/task2010/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2010/Solution.java
@@ -1,21 +1,23 @@
 package com.javarush.task.task20.task2010;
 
-/* 
+import java.io.Serializable;
+
+/*
 Как сериализовать что-то свое?
 */
 public class Solution {
-    public static class Object {
+    public static class Object implements Serializable {
         public String string1;
         public String string2;
     }
 
-    public static int stringCount;
+    public static int countStrings;
 
-    public static class String {
+    public static class String implements Serializable {
         private final int number;
 
         public String() {
-            number = ++stringCount;
+            number = ++countStrings;
         }
 
         public void print() {
@@ -24,6 +26,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-
     }
 }
+
