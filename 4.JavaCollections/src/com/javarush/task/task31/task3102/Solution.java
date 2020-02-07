c81cf2dae6cdd49a3e16e22975304f257383a2a0
TolReed <tolreed@gmail.com>
2020-02-07 17:18:02 +0200

    31.02 - Верни список всех путей к найденным файлам, путь к директориям возвращать не надо. Путь должен быть абсолютный.

--- 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -1,18 +1,48 @@
 package com.javarush.task.task31.task3102;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
-/* 
+/*
 Находим все файлы
 */
 public class Solution {
     public static List<String> getFileTree(String root) throws IOException {
-        return null;
+        List<String> files = new ArrayList<>();
+        Files.walkFileTree(new File(root).toPath(), new FileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
 
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (attrs.isRegularFile()) {
+                    files.add(new File(file.toUri()).getAbsolutePath());
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return FileVisitResult.SKIP_SUBTREE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return files;
     }
 
     public static void main(String[] args) {
-        
     }
 }
