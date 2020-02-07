03dd1a831b58dc12b5eaab920e23052bdc6da8b8
TolReed <tolreed@gmail.com>
2020-02-07 17:35:40 +0200

    31.12 - downloadFile(String urlString, Path downloadDirectory)

--- 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -5,15 +5,15 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
-/* 
+/*
 Загрузчик файлов
 */
 public class Solution {
 
     public static void main(String[] args) throws IOException {
         Path passwords = downloadFile("https://javarush.ru/testdata/secretPasswords.txt", Paths.get("D:/MyDownloads"));
-
         for (String line : Files.readAllLines(passwords)) {
             System.out.println(line);
         }
@@ -21,5 +21,12 @@ public class Solution {
 
     public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
         // implement this method
+        URL url = new URL(urlString);
+        Path tmpFile =  Files.createTempFile("dwn-", ".txt");
+        Files.copy(url.openStream(), tmpFile, StandardCopyOption.REPLACE_EXISTING);
+        Path newFile = Paths.get(downloadDirectory.toAbsolutePath().toString() + "/" + Paths.get(url.getFile()).getFileName());
+        Files.move(tmpFile, newFile, StandardCopyOption.REPLACE_EXISTING);
+        return newFile;
     }
 }
+
