71e869ac71907a9361453c0a338ece10bd67bfea
TolReed <tolreed@gmail.com>
2020-02-07 18:06:37 +0200

    20.07 - implements Serializable

--- 2.JavaCore/src/com/javarush/task/task20/task2007/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2007/Solution.java
@@ -1,13 +1,14 @@
 package com.javarush.task.task20.task2007;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-/* 
+/*
 Как сериализовать JavaRush?
 */
 public class Solution {
-    public static class JavaRush {
+    public static class JavaRush implements Serializable {
         public List<User> users = new ArrayList<>();
     }
 
