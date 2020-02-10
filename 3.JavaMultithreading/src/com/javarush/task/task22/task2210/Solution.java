da2d4ef0449ebd0b7461aa12dcba0641f4001651
TolReed <tolreed@gmail.com>
2020-02-10 16:31:28 +0200

    22.10 - StringTokenizer

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -1,13 +1,24 @@
 package com.javarush.task.task22.task2210;
 
-/* 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/*
 StringTokenizer
 */
 public class Solution {
     public static void main(String[] args) {
 
     }
+
     public static String [] getTokens(String query, String delimiter) {
-        return null;
+        StringTokenizer st = new StringTokenizer(query, delimiter);
+        List<String> list = new ArrayList<>();
+        while (st.hasMoreTokens()) {
+            list.add(st.nextToken());
+        }
+        return list.toArray(new String[0]);
     }
 }
+
