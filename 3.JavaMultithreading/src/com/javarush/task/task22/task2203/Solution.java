aeffffbb016b5cd07b50c0fc9f304dbb07730ff5
TolReed <tolreed@gmail.com>
2020-02-10 16:18:22 +0200

    22.3 - Между табуляциями

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -4,8 +4,29 @@ package com.javarush.task.task22.task2203;
 Между табуляциями
 */
 public class Solution {
-    public static String getPartOfString(String string) {
-        return null;
+    public static String getPartOfString(String string) throws TooShortStringException {
+        String result = string;
+        try {
+            int cnt = 0;
+            int index = -1;
+            for (int i = 0; i < string.length(); i++) {
+                if (string.charAt(i) == '\t') {
+                    cnt++;
+                    if (cnt > 2) {
+                        break;
+                    }
+                    index = i;
+                }
+            }
+            if (cnt < 2) {
+                throw new TooShortStringException();
+            } else {
+                result = string.substring(string.indexOf('\t') + 1, index);
+            }
+        } catch (Exception e) {
+            throw new TooShortStringException();
+        }
+        return result;
     }
 
     public static class TooShortStringException extends Exception {
@@ -15,3 +36,4 @@ public class Solution {
         System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
     }
 }
+
