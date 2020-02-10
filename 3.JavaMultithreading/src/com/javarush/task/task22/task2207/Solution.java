384d1ad8a4d65df0bc1a667c8de7e6486c8f0a9b
TolReed <tolreed@gmail.com>
2020-02-10 16:25:55 +0200

    22.07 - Обращенные слова

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
@@ -1,16 +1,59 @@
 package com.javarush.task.task22.task2207;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 
-/* 
+/*
 Обращенные слова
 */
 public class Solution {
     public static List<Pair> result = new LinkedList<>();
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+        StringBuilder sb = new StringBuilder();
+        while (fileReader.ready()) {
+            sb.append(fileReader.readLine()).append(" ");
+        }
+        fileReader.close();
+        String[] strings = sb.toString().trim().split(" ");
+        String str;
+        for (int i = 0; i < strings.length - 1; i++) {
+            boolean isExists = false;
+            if (!result.isEmpty()) {
+                for (Pair pair : result) {
+                    if (pair.first.equals(strings[i])) {
+                        isExists = true;
+                        break;
+                    }
+                }
+            }
+            if (!isExists) {
+                str = strings[i];
+                for (int j = i + 1; j < strings.length; j++) {
+                    StringBuilder sb1 = new StringBuilder(strings[j]);
+                    if (str.equals(sb1.reverse().toString())) {
+                        Pair pair = new Pair();
+                        pair.first = str;
+                        pair.second = strings[j];
+                        result.add(pair);
+                        break;
+                    }
+                }
+            }
+        }
+        /*
+        for (Pair pair : result) {
+            System.out.println(pair);
+        }
+        */
     }
 
     public static class Pair {
