fbff76edff92356fc173817c8b2cdb6d1f4456ba
TolReed <tolreed@gmail.com>
2020-02-10 15:02:51 +0200

    34.13 - Кеш на основании SoftReference

--- 4.JavaCollections/src/com/javarush/task/task34/task3413/SoftCache.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3413/SoftCache.java
@@ -9,19 +9,32 @@ public class SoftCache {
 
     public AnyObject get(Long key) {
         SoftReference<AnyObject> softReference = cacheMap.get(key);
-
         //напишите тут ваш код
+        if (softReference != null) {
+            return softReference.get();
+        }
+        return null;
     }
 
     public AnyObject put(Long key, AnyObject value) {
         SoftReference<AnyObject> softReference = cacheMap.put(key, new SoftReference<>(value));
-
+        if (softReference != null) {
+            AnyObject result = softReference.get();
+            softReference.clear();
+            return result;
+        }
         //напишите тут ваш код
+        return null;
     }
 
     public AnyObject remove(Long key) {
         SoftReference<AnyObject> softReference = cacheMap.remove(key);
-
+        if (softReference != null) {
+            AnyObject result = softReference.get();
+            softReference.clear();
+            return result;
+        }
         //напишите тут ваш код
+        return null;
     }
 }
