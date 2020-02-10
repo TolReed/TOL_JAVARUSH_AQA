b79b96cb93b029efcd4af759ff5f727c53d5055a
TolReed <tolreed@gmail.com>
2020-02-10 14:01:45 +0200

    20.15 - Переопределение сериализации
    Сделайте так, чтобы после десериализации нить runner продолжила работать.
    Ключевые слова объекта runner менять нельзя.

--- 2.JavaCore/src/com/javarush/task/task20/task2015/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2015/Solution.java
@@ -1,16 +1,22 @@
 package com.javarush.task.task20.task2015;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
 
-/* 
+/*
 Переопределение сериализации
 */
-/*public class Solution implements {
-    private Thread runner;
+public class Solution implements Serializable, Runnable{
+    private transient Thread runner;
     private int speed;
+
+    public Solution() {
+    }
 
     public Solution(int speed) {
         this.speed = speed;
@@ -19,7 +25,16 @@ import java.io.Serializable;
     }
 
     public void run() {
-        // do something here, doesn't matter what
+        // do something here, does not matter
+        try {
+            while (speed != 0) {
+                System.out.println("Current speed: " + speed);
+                TimeUnit.SECONDS.sleep(1);
+                speed--;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -29,16 +44,24 @@ import java.io.Serializable;
      private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
      Теперь сериализация/десериализация пойдет по нашему сценарию :)
      */
-    /*private void writeObject(ObjectOutputStream out) throws IOException {
+    private void writeObject(ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
     }
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
+        runner = new Thread(this);
+        runner.start();
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        Solution savedObject = new Solution(10);
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("thread.out"));
+        out.writeObject(savedObject);
+        out.close();
+        TimeUnit.SECONDS.sleep(2);
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("thread.out"));
+        Solution loadedObject = (Solution) in.readObject();
+        in.close();
     }
 }
-*/
