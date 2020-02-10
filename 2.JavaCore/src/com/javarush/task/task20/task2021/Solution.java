eb857554e818a78ada9b3b1b5672b6a4066eb5cf
TolReed <tolreed@gmail.com>
2020-02-10 14:09:03 +0200

    20.21 - Запрети сериализацию класса SubSolution используя NotSerializableException.

--- 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -7,9 +7,25 @@ import java.io.*;
 */
 public class Solution implements Serializable {
     public static class SubSolution extends Solution {
+        private void writeObject(ObjectOutputStream stream) throws IOException {
+            throw new NotSerializableException();
+        }
+
+        private void readObject(ObjectInputStream stream)
+                throws IOException, ClassNotFoundException {
+            throw new NotSerializableException();
+        }
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        SubSolution subSol = new SubSolution();
+        ObjectOutputStream out = new ObjectOutputStream(
+                new FileOutputStream("subsol.out"));
+        out.writeObject(subSol);
+        out.close();
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("subsol.out"));
+        SubSolution subSol1 = (SubSolution) in.readObject();
+        in.close();
+        System.out.println(subSol1.getClass().getSimpleName());
     }
 }
