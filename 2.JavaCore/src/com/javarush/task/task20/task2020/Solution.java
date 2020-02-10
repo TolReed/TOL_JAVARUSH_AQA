053372b2a898241f2623531c92edfba3653d96ea
TolReed <tolreed@gmail.com>
2020-02-10 14:07:26 +0200

    20.20 - fix

--- 2.JavaCore/src/com/javarush/task/task20/task2020/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2020/Solution.java
@@ -1,28 +1,33 @@
 package com.javarush.task.task20.task2020;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintStream;
+import java.io.Serializable;
 import java.util.logging.Logger;
 
-/* 
+/*
 Сериализация человека
 */
 public class Solution {
 
-    public static class Person {
+    public static class Person implements Serializable {
         String firstName;
         String lastName;
-        String fullName;
-        final String greeting;
+        transient String fullName;
+        transient final String greetingString;
         String country;
         Sex sex;
-        PrintStream outputStream;
-        Logger logger;
+        transient PrintStream outputStream;
+        transient Logger logger;
 
         Person(String firstName, String lastName, String country, Sex sex) {
             this.firstName = firstName;
             this.lastName = lastName;
             this.fullName = String.format("%s, %s", lastName, firstName);
-            this.greeting = "Hello, ";
+            this.greetingString = "Hello, ";
             this.country = country;
             this.sex = sex;
             this.outputStream = System.out;
@@ -35,7 +40,16 @@ public class Solution {
         FEMALE
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        Person person = new Person("Alex", "Bolduin",
+                "USA", Sex.MALE);
+        ObjectOutputStream out = new ObjectOutputStream(
+                new FileOutputStream("person.out"));
+        out.writeObject(person);
+        out.close();
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("person.out"));
+        Person person1 = (Person) in.readObject();
+        in.close();
+        System.out.println(person.greetingString);
     }
 }
