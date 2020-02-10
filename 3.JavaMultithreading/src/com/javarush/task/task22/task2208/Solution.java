f2b6281dd6652005407431cf9434ee6c25c97a82
TolReed <tolreed@gmail.com>
2020-02-10 16:26:40 +0200

    22.08 - Формируем WHERE

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -1,15 +1,37 @@
 package com.javarush.task.task22.task2208;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
-/* 
+/*
 Формируем WHERE
 */
 public class Solution {
     public static void main(String[] args) {
-
+        Map<String, String> query = new HashMap<>();
+        query.put("name", null);
+        query.put("country", null);
+        query.put("city", null);
+        query.put("age", null);
+        query.put("date", null);
+        System.out.println(getQuery(query));
     }
     public static String getQuery(Map<String, String> params) {
-        return null;
+        Set<String> keys = params.keySet();
+        StringBuilder sb = new StringBuilder();
+        for (String str : keys) {
+            if (params.get(str) != null) {
+                if (!params.get(str).equals("null")) {
+                    sb.append(str).append(" = '").append(params.get(str))
+                            .append("' and ");
+                }
+            }
+        }
+        if (sb.length() != 0) {
+            sb.delete(sb.lastIndexOf(" and "), sb.length());
+        }
+        return sb.toString();
     }
 }
+
