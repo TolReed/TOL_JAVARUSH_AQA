93d9da801d37f14ce5b4d2601b7449ba80e174d9
TolReed <tolreed@gmail.com>
2020-02-10 14:09:54 +0200

    20.21 - Сериализация под запретом Запрети сериализацию класса SubSolution используя NotSerializableException.

--- 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -2,9 +2,7 @@ package com.javarush.task.task20.task2021;
 
 import java.io.*;
 
-/* 
-Сериализация под запретом
-*/
+/* Сериализация под запретом */
 public class Solution implements Serializable {
     public static class SubSolution extends Solution {
         private void writeObject(ObjectOutputStream stream) throws IOException {
