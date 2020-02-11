4fde17199d6bd02e4906c9b8ff5e7875a77858b8
TolReed <tolreed@gmail.com>
2020-02-11 14:02:59 +0200

    23.06 - My name is George. I'm from Seattle in the United States.

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2306/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2306/Solution.java
@@ -2,6 +2,15 @@ package com.javarush.task.task23.task2306;
 
 /* 
 Inner 2
+В классе SuperUser метод getDescription должен учитывать страну и город, т.е. возвращать результат аналогичный следующему:
+My name is George. I'm from the USA, Seattle.
+Используй возможности иннер класса.
+Требования:
+1. Класс SuperUser должен быть приватным.
+2. Класс SuperUser должен содержать метод getDescription.
+3. Метод getDescription в классе SuperUser должен быть публичным.
+4. Метод getDescription в классе SuperUser должен возвращать строку учитывающую имя, страну и город пользователя форматированную согласно условию задачи.
+5. Метод getTrickyUser должен возвращать объект типа SuperUser.
 */
 public class Solution {
     private String country;
@@ -14,7 +23,7 @@ public class Solution {
 
     /*т.к. модификатор иннер класса private, то чтобы вызвать метод getDescription из другого внешнего класса,
     нужно обернуть его вызов в какой-то public метод*/
-    public String getUserDescription(String name) {
+    public String getDescriptionOfUser(String name) {
         return getTrickyUser(name).getDescription();
     }
 
@@ -32,15 +41,15 @@ public class Solution {
 
         //доступ к этому методу возможен только внутри класса Solution, т.к. модификатор иннер класса private
         public String getDescription() {
-            return String.format("My name is %s.", this.name);
+            return String.format("My name is %s. I'm from %s in %s.", this.name, Solution.this.city, Solution.this.country);
         }
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution("the United States", "Seattle");
+        Solution solution = new Solution("the USA", "Seattle");
         //внутри класса Solution (а сейчас мы внутри) к методу getDescription можно обращаться обоими способами
         System.out.println(solution.getTrickyUser("George").getDescription());
         //а из любого другого внешнего класса только так:
-        System.out.println(solution.getUserDescription("George"));
+        System.out.println(solution.getDescriptionOfUser("George"));
     }
 }
