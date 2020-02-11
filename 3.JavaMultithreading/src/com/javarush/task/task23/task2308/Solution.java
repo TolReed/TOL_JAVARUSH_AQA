b8832956337085729b33806302a55c9c463ca0c0
TolReed <tolreed@gmail.com>
2020-02-11 14:18:51 +0200

    23.08 - Рефакторинг, вложенные классы

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2308/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2308/Solution.java
@@ -2,35 +2,53 @@ package com.javarush.task.task23.task2308;
 
 /* 
 Рефакторинг, вложенные классы
+Отрефакторите класс Solution: вынесите все константы в public вложенный(nested) класс Constants.
+Запретите наследоваться от Constants.
+Требования:
+1. В классе Solution должен быть создан класс Constants содержащий строковые константы.
+2. Класс Constants должен быть публичным.
+3. Класс Constants должен быть объявлен с модификатором, запрещающим наследование от этого класса.
+4. В классе Constants должна существовать константа SERVER_IS_NOT_ACCESSIBLE_FOR_NOW со значением "Server is not accessible for now.".
+5. В классе Constants должна существовать константа USER_IS_NOT_AUTHORIZED со значением "User is not authorized.".
+6. В классе Constants должна существовать константа USER_IS_BANNED со значением "User is banned.".
+7. В классе Constants должна существовать константа ACCESS_IS_DENIED со значением "Access is denied.".
 */
 public class Solution {
+    public final static class Constants {
+        public final static String SERVER_IS_CURRENTLY_NOT_ACCESSIBLE = "The server is not currently accessible.";
+        public final static String USER_IS_NOT_AUTHORIZED = "User is not authorized.";
+        public final static String USER_IS_BANNED = "User is banned.";
+        public final static String ACCESS_IS_DENIED = "Access is denied.";
+
+    }
+
     public class ServerNotAccessibleException extends Exception {
         public ServerNotAccessibleException() {
-            super("The server is currently not accessible.");
+            super("Server is not accessible for now.");
         }
 
         public ServerNotAccessibleException(Throwable cause) {
-            super("The server is currently not accessible.", cause);
+            super("Server is not accessible for now.", cause);
         }
     }
 
     public class UnauthorizedUserException extends Exception {
         public UnauthorizedUserException() {
-            super("The user is not authorized.");
+            super("User is not authorized.");
         }
 
         public UnauthorizedUserException(Throwable cause) {
-            super("The user is not authorized.", cause);
+            super("User is not authorized.", cause);
         }
     }
 
     public class BannedUserException extends Exception {
         public BannedUserException() {
-            super("The user is banned.");
+            super("User is banned.");
         }
 
         public BannedUserException(Throwable cause) {
-            super("The user is banned.", cause);
+            super("User is banned.", cause);
         }
     }
 
