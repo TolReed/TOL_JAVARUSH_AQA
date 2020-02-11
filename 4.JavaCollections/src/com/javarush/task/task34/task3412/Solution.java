30379f819e4a4d1aa545a359a595ec132d406e7d
TolReed <tolreed@gmail.com>
2020-02-11 12:53:00 +0200

    34.12 - Добавление логирования в класс

--- 4.JavaCollections/src/com/javarush/task/task34/task3412/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3412/Solution.java
@@ -1,10 +1,12 @@
 package com.javarush.task.task34.task3412;
 
-/*
-Добавление логирования в класс
-*/
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Date;
 
-/*public class Solution {
+/* Добавление логирования в класс - https://github.com/vladmeh/jrt */
+
+public class Solution {
     private static final Logger logger = LoggerFactory.getLogger(Solution.class);
 
     private int value1;
@@ -12,6 +14,7 @@ package com.javarush.task.task34.task3412;
     private Date value3;
 
     public Solution(int value1, String value2, Date value3) {
+        logger.debug("Constructor: " + value1 + " " + value2 + " " + value3);
         this.value1 = value1;
         this.value2 = value2;
         this.value3 = value3;
@@ -22,42 +25,53 @@ package com.javarush.task.task34.task3412;
     }
 
     public void calculateAndSetValue3(long value) {
+        logger.trace("start calculateAndSetValue3");
+
         value -= 133;
         if (value > Integer.MAX_VALUE) {
             value1 = (int) (value / Integer.MAX_VALUE);
+            logger.debug("new value1: ", value1);
         } else {
             value1 = (int) value;
+            logger.debug("new value1: ", value1);
         }
     }
 
     public void printString() {
+        logger.trace("print value2 length");
         if (value2 != null) {
             System.out.println(value2.length());
         }
     }
 
     public void printDateAsLong() {
+        logger.trace("print date long");
         if (value3 != null) {
             System.out.println(value3.getTime());
         }
     }
 
     public void divide(int number1, int number2) {
+        logger.trace("divide number1/number2");
         try {
             System.out.println(number1 / number2);
         } catch (ArithmeticException e) {
+            logger.error("Unknown error", e);
         }
     }
 
     public void setValue1(int value1) {
         this.value1 = value1;
+        logger.debug("set value1: ", value1);
     }
 
     public void setValue2(String value2) {
         this.value2 = value2;
+        logger.debug("set value2: ", value2);
     }
 
     public void setValue3(Date value3) {
         this.value3 = value3;
+        logger.debug("set value3: ", value3);
     }
-}*/
+}
